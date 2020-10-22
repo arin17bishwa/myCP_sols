73c433f01fd72bbc9fd241a45da54db3ec5767f5
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-10-23 00:09:55 +0530

    UWCOI20

---
+++ CC/UWCOI20A.py
@@ -0,0 +1,9 @@
+s = ''
+
+for _ in range(int(input())):
+    m = -1
+    for _ in range(int(input())):
+        m = max(m, int(input()))
+    s += str(m) + '\n'
+
+print(s)
---
+++ CC/UWCOI20B.py
@@ -0,0 +1,19 @@
+st = ''
+
+
+def func(l1, n):
+    even, odd = 0, 0
+    for i in l1:
+        if i % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    return even * odd
+
+
+for _ in range(int(input())):
+    n = int(input())
+    l1 = list(map(int, input().split()))
+    st += str(func(l1, n)) + '\n'
+
+print(st)
