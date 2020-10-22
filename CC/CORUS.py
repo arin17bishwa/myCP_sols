c09b87f16d2d9003028409ec4818588e2553079c
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-10-22 21:56:54 +0530

    ADDED MAY20B

---
+++ CC/CORUS.py
@@ -0,0 +1,21 @@
+st = ''
+
+
+def func():
+    k = 0
+    for i in freq:
+        k += max(0, i - c)
+    return k
+
+
+for _ in range(int(input())):
+    n, q = map(int, input().split())
+    s = input()
+    freq = [0 for _ in range(26)]
+    for i in s:
+        freq[ord(i) - ord('a')] += 1
+    for _ in range(q):
+        c = int(input())
+        st += str(func()) + '\n'
+    # freq=[]
+print(st)
---
+++ CC/COVID19.py
@@ -0,0 +1,23 @@
+st = ''
+
+
+def func():
+    m1, m2 = n, 0
+    c = i = 1
+    while i < n:
+        if l[i] - l[i - 1] < 3:
+            c += 1
+        else:
+            m1, m2 = min(m1, c), max(m2, c)
+            c = 1
+        i += 1
+    m1, m2 = min(m1, c), max(m2, c)
+    return (str(m1) + ' ' + str(m2))
+
+
+for _ in range(int(input())):
+    n = int(input())
+    l = list(map(int, input().split()))
+    # print(l)
+    st += str(func()) + '\n'
+print(st)
