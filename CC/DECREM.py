0ac0832cfd3bf7a3b43f5848f5975a41761874c0
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-10-29 02:58:03 +0530

    DECREM.py

---
+++ CC/DECREM.py
@@ -0,0 +1,47 @@
+# region smaller_fastio
+from sys import stdin, stdout
+from os import path
+
+if path.exists('input.txt'):
+    # ------------------Sublime--------------------------------------#
+    stdin = open('input.txt', 'r')
+    stdout = open('output.txt', 'w')
+
+
+    def I():
+        return int(input())
+
+
+    def In():
+        return map(int, input().split())
+else:
+    # ------------------PYPY FAst I/o--------------------------------#
+    def I():
+        return int(stdin.readline())
+
+
+    def In():
+        return map(int, stdin.readline().split())
+
+
+    def S():
+        return stdin.readline().rstrip()
+
+
+    def Sn():
+        return stdin.readline().split(' ')
+
+
+# endregion
+
+def func(left, right):
+    if right < 2 * left:
+        return right
+    return -1
+
+
+if __name__ == '__main__':
+    st = ''
+    for _ in range(I()):
+        l, r = In()
+        print(func(l, r))
