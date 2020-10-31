1791699f185e2623b4adc48fcebc157985abf330
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-10-31 20:59:13 +0530

    changed template.cpp

--- template.cpp
+++ template.cpp
@@ -34,11 +34,12 @@ void file_IO()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
+/*
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+ */
 }
 
 
