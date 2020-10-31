73f23af42d3fea9b643b7b474de77cfac19aa6dc
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-10-31 15:20:18 +0530

    SCC0103.py

---
+++ CC/SCC0103.py
@@ -0,0 +1,52 @@
+# region smaller_fastio
+from sys import stdin, stdout
+from os import path
+
+if path.exists('input.txt'):
+    # ------------------Sublime--------------------------------------#
+    stdin = open('input.txt', 'r')
+    stdout = open('output.txt', 'w')
+
+
+    def I():
+        return int(input())
+
+
+    def In():
+        return map(int, input().split())
+else:
+    # ------------------PYPY FAst I/o--------------------------------#
+    def I():
+        return int(stdin.readline())
+
+
+    def In():
+        return map(int, stdin.readline().split())
+
+
+    def S():
+        return stdin.readline().rstrip()
+
+
+    def Sn():
+        return stdin.readline().split(' ')
+
+
+def Out(whatever):
+    return stdout.write(whatever)
+
+
+# endregion
+
+
+if __name__ == '__main__':
+    q = I()
+    answers = ['   '] * q
+    for i in range(q):
+        n = I()
+        l1 = [0] * (24 * 60 + 1)
+        for _ in range(n):
+            h, m = In()
+            l1[60 * h + m] += 1
+        answers[i] = str(max(l1))
+    Out('\n'.join(answers))
