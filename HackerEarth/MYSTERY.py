1350dfdf36972ea66b9a8ec13adf66263cc090a6
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-10-31 14:44:04 +0530

    initial HackerEarth commit added MYSTERY.py

---
+++ HackerEarth/MYSTERY.py
@@ -0,0 +1,50 @@
+# region smaller_fastio
+from sys import stdin, stdout
+from os import path
+
+if path.exists('input.txt'):
+    # ------------------Sublime--------------------------------------#
+    stdin = open('input.txt', 'r')
+    stdout = open('output.txt', 'w')
+
+
+    def I():
+        return int(input())
+
+
+    def In():
+        return map(int, input().split())
+else:
+    # ------------------PYPY FAst I/o--------------------------------#
+    def I():
+        return int(stdin.readline())
+
+
+    def In():
+        return map(int, stdin.readline().split())
+
+
+    def S():
+        return stdin.readline().rstrip()
+
+
+    def Sn():
+        return stdin.readline().split(' ')
+
+
+def Out(whatever):
+    return stdout.write(whatever)
+
+
+# endregion
+
+if __name__ == '__main__':
+    ans = []
+    while True:
+        try:
+            n = I()
+            c = 0
+            ans.append(str((n | (n - 1)) ^ n - 1))
+        except ValueError:
+            break
+    Out('\n'.join(ans))
