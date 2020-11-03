dd8809782ff6e9709effdb8f86cf4308751665cf
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-11-04 00:06:40 +0530

    added roughwork.py added CDX1604.py

---
+++ CC/CDX1604.py
@@ -0,0 +1,57 @@
+# region smaller_fastio
+from sys import stdin, stdout
+from os import path
+
+if path.exists('input.txt'):
+    # ------------------Sublime--------------------------------------#
+    stdin = open('input.txt', 'r')
+    stdout = open('output.txt', 'w')
+
+
+    def I():
+        return int(input())
+
+
+    def In():
+        return map(int, input().split())
+else:
+    # ------------------PYPY FAst I/o--------------------------------#
+    def I():
+        return int(stdin.readline())
+
+
+    def In():
+        return map(int, stdin.readline().split())
+
+
+    def S():
+        return stdin.readline().rstrip()
+
+
+    def Sn():
+        return stdin.readline().split(' ')
+
+
+def Out(whatever):
+    return stdout.write(whatever)
+
+
+# endregion
+
+def func(arr: list):
+    arr.sort()
+    n=len(arr)
+    ans=0
+    for i in range(1,n+1):
+        ans+=abs(arr[i-1]-i)
+    return ans
+
+
+if __name__ == '__main__':
+    t = I()
+    answers = [' '] * t
+    for i in range(t):
+        n = I()
+        arr = list(In())
+        answers[i] = str(func(arr))
+    print('\n'.join(answers))
