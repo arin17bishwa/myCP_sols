0d04a273e986aa4b7a14e5e668c117b171364c6c
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-11-09 01:51:59 +0530

    873A.py

---
+++ CF/873A.py
@@ -0,0 +1,46 @@
+# region smaller_fastio
+from sys import stdin, stdout
+from os import path
+
+if path.exists('input.txt'):
+    # ------------------Sublime--------------------------------------#
+    stdin = open('input.txt', 'r')
+    stdout = open('output.txt', 'w')
+
+
+    def I():
+        return int(input())
+
+
+    def In():
+        return map(int, input().split())
+else:
+    # ------------------PYPY FAst I/o--------------------------------#
+    def I():
+        return int(stdin.readline())
+
+
+    def In():
+        return map(int, stdin.readline().split())
+
+
+    def S():
+        return stdin.readline().rstrip()
+
+
+    def Sn():
+        return stdin.readline().split(' ')
+
+
+def Out(whatever):
+    return stdout.write(whatever)
+
+
+# endregion
+
+
+if __name__ == '__main__':
+    n, k, x = In()
+    arr = list(In())
+    ans = sum(arr[:n - k]) + x * k
+    Out(str(ans))
