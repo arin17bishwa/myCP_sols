6789ee3494e4c4c3bb9c283b0b058f4a1ea8f264
Bishwajit Ghosh <arin17bishwa@gmail.com>
2020-12-04 21:41:29 +0530

    modified smaller_fastio

--- template.py
+++ template.py
@@ -57,36 +57,22 @@ input = lambda: sys.stdin.readline().rstrip("\r\n")
 
 # region smaller_fastio
 from sys import stdin, stdout
-from os import path
-
-if path.exists('input.txt'):
-    # ------------------Sublime--------------------------------------#
-    stdin = open('input.txt', 'r')
-    stdout = open('output.txt', 'w')
 
 
-    def I():
-        return int(input())
+def I():
+    return int(stdin.readline())
 
 
-    def In():
-        return map(int, input().split())
-else:
-    # ------------------PYPY FAst I/o--------------------------------#
-    def I():
-        return int(stdin.readline())
+def In():
+    return map(int, stdin.readline().split())
 
 
-    def In():
-        return map(int, stdin.readline().split())
+def S():
+    return stdin.readline().rstrip()
 
 
-    def S():
-        return stdin.readline().rstrip()
-
-
-    def Sn():
-        return stdin.readline().split(' ')
+def Sn():
+    return stdin.readline().split(' ')
 
 
 def Out(whatever):
