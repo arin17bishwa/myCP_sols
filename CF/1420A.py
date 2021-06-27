a90ed3c5c9407790d5602e70a17875099231b720
Bishwajit Ghosh <arin17bishwa@gmail.com>
2021-06-27 18:37:40 +0530

    modified 1420A

--- CF/1420A.py
+++ CF/1420A.py
@@ -1,100 +1,83 @@
-# region smaller_fastio
-from sys import stdin,stdout
-from os import path
+# region fastio
+import os
+import sys
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
 
 
-if (path.exists('input.txt')):
-    #------------------Sublime--------------------------------------#
-    stdin=open('input.txt','r');stdout=open('output.txt','w');
-    def I():return (int(input()))
-    def In():return(map(int,input().split()))
-else:
-    #------------------PYPY FAst I/o--------------------------------#
-    def I():return (int(stdin.readline()))
-    def In():return(map(int,stdin.readline().split()))
+class FastIO(IOBase):
+    newlines = 0
 
-#endregion
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
 
-def mergeSort(arr, n):
-    # A temporary_Array is created to store
-    # sorted array in merge function
-    temporary_Array = [0] * n
-    return _mergeSort(arr, temporary_Array, 0, n - 1)
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
 
-
-def _mergeSort(arr, temporary_Array, left, right):
-    # A variable inv_count is used to store
-    # inversion counts in each recursive call
-
-    inv_count = 0
-
-    # We will make a recursive call if and only if
-    # we have more than one elements
-
-    if left < right:
-        # mid is calculated to divide the array into two subarrays
-        # Floor division is must in case of python
-
-        mid = (left + right) // 2
-
-        # It will calculate inversion counts in the left subarray
-
-        inv_count += _mergeSort(arr, temporary_Array, left, mid)
-
-        # It will calculate inversion counts in right subarray
-
-        inv_count += _mergeSort(arr, temporary_Array, mid + 1, right)
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
 
 
-        inv_count += merge(arr, temporary_Array, left, mid, right)
-    return inv_count
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
 
 
-def merge(arr, temporary_Array, left, mid, right):
-    i = left  #
-    j = mid + 1
-    k = left
-    inv_count = 0
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# endregion
 
 
-    while i <= mid and j <= right:
+def intArr():
+    return map(int, input().split())
 
 
-        if arr[i] <= arr[j]:
-            temporary_Array[k] = arr[i]
-            k += 1
-            i += 1
-        else:
-            temporary_Array[k] = arr[j]
-            inv_count += (mid - i + 1)
-            k += 1
-            j += 1
+def In():
+    return int(input())
 
-    while i <= mid:
-        temporary_Array[k] = arr[i]
-        k += 1
-        i += 1
 
-    while j <= right:
-        temporary_Array[k] = arr[j]
-        k += 1
-        j += 1
+def func():
+    n=In()
+    arr=list(intArr())
+    for i in range(1,n):
+        if arr[i-1]<=arr[i]:
+            return 'YES'
+    return 'NO'
 
-    return inv_count
 
 def main():
-    t=I()
-    st=''
-    for _ in range(t):
-        n=I()
-        l=list(In())
-        k=mergeSort(l,n)
-        if k<=(((n-1)*n)/2)-1:
-            ans='YES'
-        else:
-            ans='NO'
-        st='\n'.join((st,ans))
-    print(st[1:])
+    for _ in range(In()):
+        print(func())
+    return
+
+
 if __name__ == '__main__':
     main()
