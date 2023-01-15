ead26621b2948110ab752436e14293b5dcab0c0b
bishwajit <arin17bishwa@gmail.com>
2023-01-15 23:38:13 +0530

    done

---
+++ CC/NODRUGS.py
@@ -0,0 +1,95 @@
+# region fastio
+import os
+import sys
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+
+# endregion
+
+
+def intArr():
+    return map(int, input().split())
+
+
+def In():
+    return int(input())
+
+
+def func():
+    n, k, l = intArr()
+    arr = tuple(intArr())
+    friend_speed = arr[-1]
+    top_speed = max(arr)
+    if friend_speed == top_speed:
+        if arr.count(friend_speed) == 1:
+            return 1
+        if l > 1:
+            return 1
+        return 0
+    if k < 1:
+        return 0
+    diff = top_speed - friend_speed
+    no_dose = (diff // k)
+    if not no_dose * k + friend_speed > top_speed:
+        no_dose += 1
+    return no_dose < l
+
+
+def main():
+    for _ in range(In()):
+        print('Yes' if func() else 'No')
+    return
+
+
+if __name__ == '__main__':
+    main()
