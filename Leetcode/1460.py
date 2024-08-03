09092fed2513213983e55e2cdde4a9b2243b298f
Bishwajit Ghosh <b-ghosh@ti.com>
2024-08-04 00:11:14 +0530

    https://leetcode.com/problems/make-two-arrays-equal-by-reversing-subarrays/submissions/1342820518

---
+++ Leetcode/1460.py
@@ -0,0 +1,7 @@
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
+        return Counter(target) == Counter(arr)
