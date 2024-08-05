92786489312b90f321d685d275f7a5d32b1b3b71
bishwajit <arin17bishwa@gmail.com>
2024-08-05 06:59:24 +0530

    https://www.codechef.com/viewsolution/1077310433

--- CC/CNDY.py
+++ CC/CNDY.py
@@ -1,4 +1,7 @@
-# region fastio
+from typing import Iterable, Callable
+from collections import Counter
+
+# region fast io
 import os
 import sys
 from io import BytesIO, IOBase
@@ -25,7 +28,7 @@ class FastIO(IOBase):
         self.newlines = 0
         return self.buffer.read()
 
-    def readline(self):
+    def readline(self, *args, **kwargs) -> bytes:
         while self.newlines == 0:
             b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
             self.newlines = b.count(b"\n") + (not b)
@@ -53,35 +56,43 @@ class IOWrapper(IOBase):
 sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
 input = lambda: sys.stdin.readline().rstrip("\r\n")
 
-
 # endregion
 
 
-def intArr():
+def int_arr() -> Iterable[int]:
     return map(int, input().split())
 
 
-def In():
+def iin() -> int:
     return int(input())
 
 
+def yn_dec(function) -> Callable:
+    letter_case: str = "upper"
+
+    def inner1(*args, **kwargs) -> str:
+        res = function(*args, **kwargs)
+        ans = "yes" if res else "no"
+        if letter_case == "upper":
+            return ans.upper()
+        if letter_case == "title":
+            return ans.title()
+        if letter_case == "lower":
+            return ans.lower()
+        return res
+
+    return inner1
+
+
+@yn_dec
 def func():
-    n = int(input())
-    l = list(map(int, input().split()))
-    c = 0
-    for j in l:
-        k = l.count(j)
-        if k >= 3:
-            c += 1
-    if c > 1:
-        return 0
-    return 1
+    _ = input()
+    return not any(i > 2 for i in Counter(int_arr()).values())
 
 
 def main():
-    for _ in range(In()):
-        print("YES" if func() else "NO")
-    return
+    for _ in range(iin()):
+        print(func())
 
 
 if __name__ == "__main__":
