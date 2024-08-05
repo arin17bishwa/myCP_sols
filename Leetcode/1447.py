9e9fe13c3b08c0375eccc0d347eab81d0705b6ad
bishwajit <arin17bishwa@gmail.com>
2024-08-05 21:19:16 +0530

    https://leetcode.com/problems/simplified-fractions/submissions/1345536003

--- Leetcode/1447.py
+++ Leetcode/1447.py
@@ -1,18 +1,19 @@
-from typing import List, Set, Tuple
+from typing import List, Tuple
 from math import gcd
 
 
 class Solution:
     def simplifiedFractions(self, n: int) -> List[str]:
-        ans: Set[str] = set()
+        ans: List[str] = []
 
         for denominator in range(2, n + 1):
             for numerator in range(1, denominator):
                 reduced_numerator, reduced_denominator = self.reduce_fraction(
                     numerator, denominator
                 )
-                ans.add(f"{reduced_numerator}/{reduced_denominator}")
-        return list(ans)
+                if reduced_denominator == denominator:
+                    ans.append(f"{reduced_numerator}/{reduced_denominator}")
+        return ans
 
     @staticmethod
     def reduce_fraction(numerator: int, denominator: int) -> Tuple[int, int]:
