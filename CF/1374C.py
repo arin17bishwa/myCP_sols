e76c1942232a84fd51e11bd3f1c171fd90072395
Bishwajit Ghosh <arin17bishwa@gmail.com>
2024-08-22 23:04:05 +0530

    https://codeforces.com/contest/1374/submission/277688020

--- CF/1374C.py
+++ CF/1374C.py
@@ -1,61 +1,106 @@
-# region smaller_fastio
-from sys import stdin, stdout
-from os import path
+from typing import Iterable, Callable
+from collections import deque
 
-if (path.exists('input.txt')):
-    # ------------------Sublime--------------------------------------#
-    stdin = open('input.txt', 'r');
-    stdout = open('output.txt', 'w');
+# region fast io
+import os
+import sys
+from io import BytesIO, IOBase
 
-
-    def I():
-        return (int(input()))
+BUFSIZE = 8192
 
 
-    def In():
-        return (map(int, input().split()))
-else:
-    # ------------------PYPY FAst I/o--------------------------------#
-    def I():
-        return (int(stdin.readline()))
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self, *args, **kwargs) -> bytes:
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
 
 
-    def In():
-        return (map(int, stdin.readline().split()))
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
 
 
-    def S():
-        return (stdin.readline().rstrip())
-
-
-    def Sn():
-        return (stdin.readline().split(' '))
-
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
 
 # endregion
 
-from collections import deque
 
-def f(s):
-    n=len(s)
-    d=deque()
-    for i in s:
-        if i=='(':
-            d.append(i)
-        if i==')':
-            if len(d)<1:pass
+def int_arr() -> Iterable[int]:
+    return map(int, input().split())
 
-    pass
+
+def iin() -> int:
+    return int(input())
+
+
+def yn_dec(function) -> Callable:
+    letter_case: str = "upper"
+
+    def inner1(*args, **kwargs) -> str:
+        res = function(*args, **kwargs)
+        ans = "yes" if res else "no"
+        if letter_case == "upper":
+            return ans.upper()
+        if letter_case == "title":
+            return ans.title()
+        if letter_case == "lower":
+            return ans.lower()
+        return res
+
+    return inner1
+
 
 def func():
-    st=''
-    t=I()
-    for _ in range(t):
-        n=I()
-        s=S()
-        st='\n'.join((st,str(f(s))))
+    _ = iin()
+    d = deque()
+    for ch in input():
+        if not d or ch == "(":
+            d.append(ch)
+        elif d[-1] == "(":
+            d.pop()
+        else:
+            d.append(")")
+    return len(d) // 2
 
-    stdout.write(st.lstrip())
 
-if __name__ == '__main__':
-    func()
+def main():
+    for _ in range(iin()):
+        print(func())
+
+
+if __name__ == "__main__":
+    main()
