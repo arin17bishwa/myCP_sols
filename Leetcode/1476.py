4a26d19ea2d8c24f94d07a042fc1c6ae3dbb8d53
bishwajit <arin17bishwa@gmail.com>
2024-08-25 23:51:45 +0530

    https://leetcode.com/problems/subrectangle-queries/submissions/1368178337

--- Leetcode/1476.py
+++ Leetcode/1476.py
@@ -7,16 +7,11 @@ class SubrectangleQueries:
         self.matrix = rectangle
 
     def updateSubrectangle(
-            self, row1: int, col1: int, row2: int, col2: int, newValue: int
+            self, row1: int, col1: int, row2: int, col2: int, new_value: int
     ) -> None:
         for i in range(row1, row2 + 1):
             for j in range(col1, col2 + 1):
-                self.matrix[i][j] = newValue
+                self.matrix[i][j] = new_value
 
     def getValue(self, row: int, col: int) -> int:
         return self.matrix[row][col]
-
-# Your SubrectangleQueries object will be instantiated and called as such:
-# obj = SubrectangleQueries(rectangle)
-# obj.updateSubrectangle(row1,col1,row2,col2,newValue)
-# param_2 = obj.getValue(row,col)
