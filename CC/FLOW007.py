e3477950c0a27efdca23b3eaa8c4236ba80f98dd
Bishwajit Ghosh <arin17bishwa@gmail.com>
2024-10-03 23:40:38 +0530

    https://www.codechef.com/viewsolution/1095645455
    Took 16 minutes

--- CC/FLOW007.py
+++ CC/FLOW007.py
@@ -1,4 +1,6 @@
-# region fastio
+from typing import Iterable, Callable
+
+# region fast io
 import os
 import sys
 from io import BytesIO, IOBase
@@ -25,7 +27,7 @@ class FastIO(IOBase):
         self.newlines = 0
         return self.buffer.read()
 
-    def readline(self):
+    def readline(self, *args, **kwargs) -> bytes:
         while self.newlines == 0:
             b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
             self.newlines = b.count(b"\n") + (not b)
@@ -56,18 +58,39 @@ input = lambda: sys.stdin.readline().rstrip("\r\n")
 # endregion
 
 
+def int_arr() -> Iterable[int]:
+    return map(int, input().split())
+
+
+def iin() -> int:
+    return int(input())
+
+
+def yn_dec(function) -> Callable:
+    letter_case: str = "upper"
+
+    def inner1(*args, **kwargs) -> str:
+        res = function(*args, **kwargs)
+        ans = "yes" if res else "no"
+        if letter_case == "upper":
+            return ans.upper()
+        if letter_case == "title":
+            return ans.title()
+        if letter_case == "lower":
+            return ans.lower()
+        return res
+
+    return inner1
+
+
+def func():
+    return int(input()[::-1])
+
+
 def main():
-    for _ in range(int(input())):
-        print(int(input()[::-1]))
-    return
+    for _ in range(iin()):
+        print(func())
 
 
 if __name__ == "__main__":
     main()
-
-"""
-One liner:
-
-    print('\n'.join([str(int(input()[::-1])) for _ in range(int(input()))]))
-    
-"""
