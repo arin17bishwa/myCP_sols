2ebaf96286763daa9a73ebbe2e0afc2f06d732b6
Bishwajit Ghosh <arin17bishwa@gmail.com>
2024-12-05 00:30:05 +0530

    AC
    Took 9 minutes
    
    
    Took 1 hour 50 minutes

---
+++ GFG/sort-an-array-of-0s-1s-and-2s4231.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    # Function to sort an array of 0s, 1s, and 2s
+    def sort012(self, arr: List[int]):
+        # code here
+        arr.sort()
+        return arr
