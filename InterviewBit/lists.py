9d465ab20bceae36440995823d16f6cfed7d60e7
Bishwajit Ghosh <arin17bishwa@gmail.com>
2024-12-04 23:21:52 +0530

    AC
    Took 3 minutes

---
+++ InterviewBit/lists.py
@@ -0,0 +1,13 @@
+def main():
+    names = []
+    # YOUR CODE GOES HERE
+    names.append("Robin")
+    names.append("Aman")
+    names.append("Rahul")
+
+    print(names)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
