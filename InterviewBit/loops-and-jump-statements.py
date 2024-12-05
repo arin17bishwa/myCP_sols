3a465643de679d2c9093269f38e833094e1e7ef7
Bishwajit Ghosh <arin17bishwa@gmail.com>
2024-12-05 22:18:13 +0530

    AC
    Took 4 minutes

---
+++ InterviewBit/loops-and-jump-statements.py
@@ -0,0 +1,12 @@
+def main():
+    N = int(input())
+    # YOUR CODE GOES HERE
+    for i in range(N):
+        if i % 2 == 1:
+            print(i)
+
+    return 0
+
+
+if __name__ == "__main__":
+    main()
