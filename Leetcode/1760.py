f3a56a660202fcf3fb68166d3d33b51e6ee38917
bishwajit <arin17bishwa@gmail.com>
2024-12-07 14:55:44 +0530

    https://leetcode.com/problems/minimum-limit-of-balls-in-a-bag/submissions/1472475361

--- Leetcode/1760.py
+++ Leetcode/1760.py
@@ -6,20 +6,14 @@ class Solution:
     def minimumSize(self, nums: List[int], maxOperations: int) -> int:
         arr = nums
         low = 1
-        ans = high = max(arr)
+        ans = high = 10 ** 9
 
         while low <= high:
             mid = (low + high) >> 1
-            total_ops = sum(map(lambda x: self.get_number_of_ops(x, mid), arr))
+            total_ops = sum(map(lambda x: max(0, ceil(x / mid) - 1), arr))
             if total_ops > maxOperations:
                 low = mid + 1
             else:
                 ans = mid
                 high = mid - 1
         return ans
-
-    @staticmethod
-    def get_number_of_ops(start: int, target_max: int) -> int:
-        if start <= target_max or start <= 1:
-            return 0
-        return max(0, ceil(start / target_max) - 1)
