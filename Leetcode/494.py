cd8385ba17672af8b91273ff0fc157cedad8921b
bishwajit <arin17bishwa@gmail.com>
2024-12-26 17:09:56 +0530

    https://leetcode.com/problems/target-sum/submissions/1488808125

--- Leetcode/494.py
+++ Leetcode/494.py
@@ -1,3 +1,4 @@
+from functools import cache
 from typing import List
 
 
@@ -5,16 +6,15 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         arr = nums
         n = len(arr)
-        ans: int = 0
 
-        for mask in range(1 << n):
-            curr: int = 0
-            for i in range(n):
-                if mask & (1 << i):
-                    mul = 1
-                else:
-                    mul = -1
-                curr += mul * arr[i]
-            ans += curr == target
+        @cache
+        def func(sub_target: int, start: int = 0) -> int:
+            if start == n - 1:
+                return (arr[-1], -arr[-1]).count(sub_target)
+            curr = arr[start]
+            ways = func(sub_target - curr, start + 1) + func(
+                sub_target + curr, start + 1
+            )
+            return ways
 
-        return ans
+        return int(func(target, 0))
