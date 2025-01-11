01f78cd51647e61a01d7b1e8c1e8e01ee2bdf9b1
bishwajit <arin17bishwa@gmail.com>
2025-01-12 04:12:28 +0530

    https://leetcode.com/problems/construct-k-palindrome-strings/submissions/1505536633

--- Leetcode/1400.py
+++ Leetcode/1400.py
@@ -3,6 +3,4 @@ from collections import Counter
 
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        freq = Counter(s)
-        odds = sum(v % 2 for v in freq.values())
-        return len(s) >= k >= odds
+        return len(s) >= k >= sum(v & 1 != 0 for v in Counter(s).values())
