dde8005497fd32f70338b96978eca07559aa206a
bishwajit <arin17bishwa@gmail.com>
2025-01-12 15:55:47 +0530

    https://leetcode.com/problems/minimum-operations-to-make-columns-strictly-increasing/submissions/1506070359

--- Leetcode/3402.py
+++ Leetcode/3402.py
@@ -3,12 +3,14 @@ from typing import List
 
 class Solution:
     def minimumOperations(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
         ans = 0
+        m, n = len(grid), len(grid[0])
+
         for j in range(n):
-            last = grid[0][j] - 1
-            for i in range(m):
-                req = max(last + 1, grid[i][j])
-                ans += abs(grid[i][j] - req)
-                last = req
+            last_val = grid[0][j]
+
+            for i in range(1, m):
+                new_val = max(grid[i][j], last_val + 1)
+                ans += new_val - grid[i][j]
+                last_val = new_val
         return ans
