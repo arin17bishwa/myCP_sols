32b1c40d7e8743a944c82d6a3bc4ee8d4583fcc0
Bishwajit Ghosh <arin17bishwa@gmail.com>
2025-01-20 21:09:17 +0530

    https://leetcode.com/problems/first-completely-painted-row-or-column/submissions/1514722980
    Took 6 minutes

--- Leetcode/2661.py
+++ Leetcode/2661.py
@@ -1,12 +1,15 @@
-from typing import List, Tuple, Dict
+from typing import List, Tuple
 
 
 class Solution:
     def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
         m, n = len(mat), len(mat[0])
-        indices: Dict[int, Tuple[int, int]] = {
-            mat[i][j]: (i, j) for i in range(m) for j in range(n)
-        }
+
+        indices: List[Tuple[int, int]] = [(0, 0)] * (m * n + 2)
+        for i in range(m):
+            for j in range(n):
+                indices[mat[i][j]] = (i, j)
+
         row_freq = [0] * m
         column_freq = [0] * n
 
@@ -14,6 +17,5 @@ class Solution:
             i, j = indices[x]
             row_freq[i] += 1
             column_freq[j] += 1
-
             if row_freq[i] == n or column_freq[j] == m:
                 return idx
