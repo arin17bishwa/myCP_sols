39f0e6282c8403c57559dfbc93d75af52355030d
Bishwajit Ghosh <arin17bishwa@gmail.com>
2025-01-24 19:16:15 +0530

    https://leetcode.com/problems/contains-duplicate/submissions/1519066796
    Took 2 minutes

--- Leetcode/217.py
+++ Leetcode/217.py
@@ -1,12 +1,7 @@
-from collections import defaultdict
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        freq = defaultdict(int)
-        for i in nums:
-            freq[i] += 1
-            if freq[i] > 1:
-                return True
-        return False
+        return any(i > 1 for i in Counter(nums).values())
