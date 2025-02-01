3b054ea8dbb35bf629d32ecf09b3858830ce1eca
bishwajit <arin17bishwa@gmail.com>
2025-02-01 11:06:35 +0530

    https://leetcode.com/problems/special-array-i/submissions/1527069521

--- Leetcode/3151.py
+++ Leetcode/3151.py
@@ -6,6 +6,6 @@ class Solution:
         arr = nums
         n = len(arr)
         for i in range(1, n):
-            if arr[i - 1] & 1 == arr[i] & 1:
+            if (arr[i - 1] ^ arr[i]) & 1 == 0:
                 return False
         return True
