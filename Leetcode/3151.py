3b23d39eabcb59f61c14313c1f1c90019bd3b8e9
bishwajit <arin17bishwa@gmail.com>
2025-02-01 11:04:33 +0530

    https://leetcode.com/problems/special-array-i/submissions/1527067768

--- Leetcode/3151.py
+++ Leetcode/3151.py
@@ -5,9 +5,7 @@ class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
         arr = nums
         n = len(arr)
-        prev = arr[0]
         for i in range(1, n):
-            if prev & 1 == arr[i] & 1:
+            if arr[i - 1] & 1 == arr[i] & 1:
                 return False
-            prev = arr[i]
         return True
