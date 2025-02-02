7ddf26b24dda107eb6863f103618dd7b2c2d4023
Bishwajit Ghosh <arin17bishwa@gmail.com>
2025-02-02 10:15:57 +0530

    https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/submissions/1528248231
    Took 51 seconds

--- Leetcode/80.py
+++ Leetcode/80.py
@@ -12,27 +12,11 @@ class Solution:
         pointer = 2
 
         while pointer < n:
-            # print(curr_idx, pointer)
             while pointer < n and arr[pointer] == arr[curr_idx - 2]:
                 pointer += 1
             if pointer >= n:
                 break
-
             arr[curr_idx] = arr[pointer]
             curr_idx += 1
             pointer += 1
         return curr_idx
-
-
-def main():
-    obj = Solution()
-
-    arr = [1, 1, 1, 2, 2, 3]
-
-    print(arr)
-    ans = obj.removeDuplicates(arr)
-    print(arr)
-
-
-if __name__ == "__main__":
-    main()
