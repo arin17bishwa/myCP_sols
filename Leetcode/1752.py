94757d597656304db351571db3bb5cc6b7dc8bdc
Bishwajit Ghosh <arin17bishwa@gmail.com>
2025-02-02 09:31:48 +0530

    https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/submissions/1528212859
    Took 1 minute

--- Leetcode/1752.py
+++ Leetcode/1752.py
@@ -5,15 +5,12 @@ class Solution:
     def check(self, nums: List[int]) -> bool:
         arr = nums
         n = len(arr)
-        rotations: List[int] = []
-        first = None
+        rotations: int = 0
         for i in range(1, n):
-            if first is None:
-                first = i
             if arr[i - 1] > arr[i]:
                 if rotations:
                     return False
-                rotations = [first, arr[i - 1]]
+                rotations += 1
                 if not arr[0] >= arr[-1]:
                     return False
         return True
