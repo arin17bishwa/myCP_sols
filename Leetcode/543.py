0692d645055771f5f85b5a169b4ce7910cd614a2
Bishwajit Ghosh <arin17bishwa@gmail.com>
2025-02-05 18:55:45 +0530

    https://leetcode.com/problems/diameter-of-binary-tree/submissions/1532346473
    Took 32 minutes

---
+++ Leetcode/543.py
@@ -0,0 +1,24 @@
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+        def dfs(node: Optional[TreeNode]):
+            if not node:
+                return -1, -1
+            left_self, left_mixed = dfs(node.left)
+            right_self, right_mixed = dfs(node.right)
+            return (
+                max(left_self, right_self, left_mixed + right_mixed + 2),
+                max(left_mixed, right_mixed) + 1,
+            )
+
+        return max(dfs(root))
