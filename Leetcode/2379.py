d881f1b5d031cb5db503ecd93ce1df524c3fa49e
Bishwajit Ghosh <arin17bishwa@gmail.com>
2025-03-08 07:12:28 +0530

    https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/submissions/1566517353
    Took 2 minutes

--- Leetcode/2379.py
+++ Leetcode/2379.py
@@ -10,26 +10,4 @@ class Solution:
         for i in range(1, n + 1):
             cost[i] += cost[i - 1]
 
-        return min([cost[i] - cost[i - k] for i in range(k, n + 1)])
-
-
-def main():
-    obj = Solution()
-
-    s = "WBBWWBBWBW"
-    k = 7
-
-    s = "WBWW"
-    k = 2
-
-    s = "WWBBBWBBBBBWWBWWWB"
-    k = 16
-    # print(len(s))
-
-    ans = obj.minimumRecolors(s, k)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
+        return min(cost[i] - cost[i - k] for i in range(k, n + 1))
