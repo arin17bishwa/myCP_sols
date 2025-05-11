98529d8f6fdcf627be84fd2fa387a26ed0df99f5
bishwajit <arin17bishwa@gmail.com>
2025-05-11 22:25:01 +0530

    https://leetcode.com/problems/three-consecutive-odds/submissions/1631210961

---
+++ Leetcode/1550.go
@@ -0,0 +1,12 @@
+package Leetcode
+
+func threeConsecutiveOdds(arr []int) bool {
+	n := len(arr)
+	for i := 0; i < n-2; i++ {
+		if arr[i]%2 == 1 && arr[i+1]%2 == 1 && arr[i+2]%2 == 1 {
+			return true
+		}
+	}
+	return false
+
+}
