128113f16181116933f797d12d8226bd6e1bffe8
bishwajit <arin17bishwa@gmail.com>
2025-06-09 20:10:18 +0530

    https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/submissions/1658729304

---
+++ Leetcode/440.py
@@ -0,0 +1,27 @@
+
+
+
+class Solution:
+    def findKthNumber(self, n: int, k: int) -> int:
+        k -= 1
+        curr = 1
+
+        while k > 0:
+            steps = self.count_steps(n, curr)
+            if steps <= k:
+                k -= steps
+                curr += 1
+            else:
+                curr *= 10
+                k -= 1
+        return curr
+
+    @staticmethod
+    def count_steps(n: int, curr: int):
+        steps = 0
+        nxt = curr + 1
+        while curr <= n:
+            steps += min(n + 1, nxt) - curr
+            curr *= 10
+            nxt *= 10
+        return steps
