d33cb12a1ee352c984e6bddc0794836c18645fa5
bishwajit <arin17bishwa@gmail.com>
2025-06-12 21:36:18 +0530

    AC

--- GFG/k-closest-elements3619.py
+++ GFG/k-closest-elements3619.py
@@ -1,4 +1,3 @@
 class Solution:
     def printKClosest(self, arr: list[int], k: int, x: int) -> list[int]:
-        _ans = sorted((abs(i - x), -i) for i in arr if i != x)
-        return [-i[1] for i in _ans[:k]]
+        return [-i[1] for i in sorted((abs(i - x), -i) for i in arr if i != x)[:k]]
