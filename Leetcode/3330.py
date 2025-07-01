1e44a2d234621f0d65577f9de3b6c5ad0234920a
bishwajit <arin17bishwa@gmail.com>
2025-07-01 16:35:33 +0530

    https://leetcode.com/problems/find-the-original-typed-string-i/submissions/1682574475

--- Leetcode/3330.py
+++ Leetcode/3330.py
@@ -3,15 +3,13 @@ from collections import defaultdict
 
 class Solution:
     def possibleStringCount(self, word: str) -> int:
-        groups = defaultdict(int)
-        groups[0] = 1
-        ans = last = 0
         n = len(word)
+        ans = 0
+        last_cnt = 0
         for i in range(1, n):
-            if word[i] != word[i - 1]:
-                last = i
-            groups[last] += 1
-
-        for v in groups.values():
-            ans += v - 1
-        return ans + 1
+            if word[i] == word[i - 1]:
+                last_cnt += 1
+            else:
+                ans += last_cnt
+                last_cnt = 0
+        return ans + last_cnt + 1
