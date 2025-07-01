10879af7061889d636560fec7d88b1e5bdf61a76
bishwajit <arin17bishwa@gmail.com>
2025-07-01 23:46:18 +0530

    https://leetcode.com/problems/rotate-image/submissions/1683034306

--- Leetcode/48.py
+++ Leetcode/48.py
@@ -9,7 +9,11 @@ class Solution:
         n = len(matrix)
 
         for i in range(n):
-            for j in range(i + 1, n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
             for j in range(n >> 1):
                 matrix[i][j], matrix[i][n - 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
+        for i in range(n):
+            for j in range(n - i - 1):
+                matrix[i][j], matrix[n - 1 - j][n - 1 - i] = (
+                    matrix[n - 1 - j][n - 1 - i],
+                    matrix[i][j],
+                )
