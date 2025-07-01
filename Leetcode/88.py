4ca7bd038cbb17bf8f62050e924267c23de742f4
bishwajit <arin17bishwa@gmail.com>
2025-07-02 01:08:58 +0530

    https://leetcode.com/problems/merge-sorted-array/submissions/1683113386

--- Leetcode/88.py
+++ Leetcode/88.py
@@ -6,22 +6,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        idx = m + n - 1
+        k = m + n - 1
         m -= 1
         n -= 1
         while m >= 0 and n >= 0:
             if nums1[m] >= nums2[n]:
-                nums1[idx] = nums1[m]
+                nums1[k] = nums1[m]
                 m -= 1
             else:
-                nums1[idx] = nums2[n]
+                nums1[k] = nums2[n]
                 n -= 1
-            idx -= 1
-        while m >= 0:
-            nums1[idx] = nums1[m]
-            idx -= 1
-            m -= 1
+            k -= 1
+
         while n >= 0:
-            nums1[idx] = nums2[n]
-            idx -= 1
+            nums1[k] = nums2[n]
+            k -= 1
             n -= 1
