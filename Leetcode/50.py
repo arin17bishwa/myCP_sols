46e44518a00f9fd91195fa8e51a7d5dcdd5f1bfa
bishwajit <arin17bishwa@gmail.com>
2025-07-02 21:25:47 +0530

    https://leetcode.com/problems/powx-n/submissions/1684037717

--- Leetcode/50.py
+++ Leetcode/50.py
@@ -1,10 +1,18 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        ans = 1
+        if n<0:
+            x=1/x
+            n=-n
+        def binary_exp(b:float,e:int):
+            if e==0:
+                return 1
+            if e==1:
+                return b
 
-        for _ in range(abs(n)):
-            if n > 0:
-                ans *= x
+            if e&1:
+                return b*binary_exp(b,e-1)
             else:
-                ans /= x
-        return ans
+                t=binary_exp(b,e>>1)
+                return t*t
+
+        return binary_exp(x,n)
