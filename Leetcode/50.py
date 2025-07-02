2c28480e88e7d8107abd3317057e57ffa540cf2b
bishwajit <arin17bishwa@gmail.com>
2025-07-02 21:27:06 +0530

    https://leetcode.com/problems/powx-n/submissions/1684039250

--- Leetcode/50.py
+++ Leetcode/50.py
@@ -1,18 +1,23 @@
+from functools import cache
+
+
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if n<0:
-            x=1/x
-            n=-n
-        def binary_exp(b:float,e:int):
-            if e==0:
+        if n < 0:
+            x = 1 / x
+            n = -n
+
+        @cache
+        def binary_exp(b: float, e: int):
+            if e == 0:
                 return 1
-            if e==1:
+            if e == 1:
                 return b
 
-            if e&1:
-                return b*binary_exp(b,e-1)
+            if e & 1:
+                return b * binary_exp(b, e - 1)
             else:
-                t=binary_exp(b,e>>1)
-                return t*t
+                t = binary_exp(b, e >> 1)
+                return binary_exp(b, e >> 1) * binary_exp(b, e >> 1)
 
-        return binary_exp(x,n)
+        return binary_exp(x, n)
