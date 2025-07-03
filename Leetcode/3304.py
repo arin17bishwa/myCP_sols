aff6c49e975ec0d9770c4dd019cde259e91ee627
bishwajit <arin17bishwa@gmail.com>
2025-07-03 12:33:48 +0530

    https://leetcode.com/problems/find-the-k-th-character-in-string-game-i/submissions/1684722882

--- Leetcode/3304.py
+++ Leetcode/3304.py
@@ -2,10 +2,6 @@ class Solution:
     def kthCharacter(self, k: int) -> str:
         ans = ["a"]
 
-        def next_chars(arr: list[str]) -> list[str]:
-            transformed = [chr(((ord(ch) - 96) % 26) + 97) for ch in arr]
-            return transformed
-
         while len(ans) < k:
-            ans.extend(next_chars(arr=ans))
+            ans.extend([chr(((ord(ch) - 96) % 26) + 97) for ch in ans])
         return ans[k - 1]
