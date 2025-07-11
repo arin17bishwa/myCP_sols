2495789da0d0d76343dd553707041ed44f49d7e5
bishwajit <arin17bishwa@gmail.com>
2025-07-11 14:27:20 +0530

    https://leetcode.com/problems/min-stack/submissions/1693675144 much more memory utilization

--- Leetcode/155.py
+++ Leetcode/155.py
@@ -4,24 +4,21 @@ from collections import deque
 class MinStack:
 
     def __init__(self):
-        self.stack: deque[int] = deque()
-        self.min_stack: deque[int] = deque()
+        self.stack: deque[tuple[int, int]] = deque()
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        if not self.min_stack or self.min_stack[-1] >= val:
-            self.min_stack.append(val)
+        self.stack.append(
+            (val, val) if not self.stack else (val, min(val, self.stack[-1][1]))
+        )
 
     def pop(self) -> None:
-        pop = self.stack.pop()
-        if self.min_stack[-1] == pop:
-            self.min_stack.pop()
+        self.stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.stack[-1][0]
 
     def getMin(self) -> int:
-        return self.min_stack[-1]
+        return self.stack[-1][1]
 
 
 # Your MinStack object will be instantiated and called as such:
