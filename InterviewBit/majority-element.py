fca3b72a90f42b76b265d636f67b97072678ea0f
Bishwajit Ghosh <arin17bishwa@gmail.com>
2025-07-12 00:01:40 +0530

    AC optimal
    Took 3 minutes

--- InterviewBit/majority-element.py
+++ InterviewBit/majority-element.py
@@ -1,13 +1,17 @@
-from collections import Counter
 from typing import List
 
 
 class Solution:
-    # @param A : tuple of integers
-    # @return an integer
     def majorityElement(self, arr: List[int]) -> int:
         n = len(arr)
-        freq = Counter(arr)
-        for k, v in freq.items():
-            if v > n // 2:
-                return k
+        candidate = arr[0]
+        current_freq = 1
+        for i in range(1, n):
+            if arr[i] == candidate:
+                current_freq += 1
+            else:
+                current_freq -= 1
+            if current_freq <= 0:
+                candidate = arr[i]
+                current_freq = 1
+        return candidate
