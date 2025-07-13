5e528c9b59847e36e064704c8b046b9341c29678
bishwajit <arin17bishwa@gmail.com>
2025-07-13 12:37:23 +0530

    https://leetcode.com/problems/product-of-the-last-k-numbers/submissions/1696093420
    personal approach; doesn't rely on the guarantee that there will be at least k elements when calling getProduct

--- Leetcode/1352.py
+++ Leetcode/1352.py
@@ -26,26 +26,3 @@ class ProductOfNumbers:
 # obj = ProductOfNumbers()
 # obj.add(num)
 # param_2 = obj.getProduct(k)
-
-
-def main():
-    obj = ProductOfNumbers()
-
-    obj.add(1)
-    print(obj.getProduct(1))
-
-    for i in [3, 0, 2, 5, 4]:
-        obj.add(i)
-
-    print(obj.prefix_product)
-
-    for i in [2, 3, 4]:
-        print(obj.getProduct(i))
-
-    obj.add(8)
-
-    print(obj.getProduct(2))
-
-
-if __name__ == "__main__":
-    main()
