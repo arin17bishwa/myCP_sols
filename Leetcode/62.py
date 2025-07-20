ba187fc455658ed3037bffbc43d14dca68c9478a
bishwajit <arin17bishwa@gmail.com>
2025-07-21 02:07:05 +0530

    https://leetcode.com/problems/unique-paths/submissions/1705206185

---
+++ Leetcode/62.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[1] * n for _ in range(m)]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
+        return grid[-1][-1]
