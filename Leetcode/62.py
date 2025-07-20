0ddf54ea147530182d602944622f2cd44c1763a6
bishwajit <arin17bishwa@gmail.com>
2025-07-21 02:10:55 +0530

    https://leetcode.com/problems/unique-paths/submissions/1705208523 O(n) space

--- Leetcode/62.py
+++ Leetcode/62.py
@@ -1,8 +1,11 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        grid = [[1] * n for _ in range(m)]
 
-        for i in range(1, m):
+        upper_row = [1] * n
+        current_row = [1] * n
+
+        for _ in range(m - 1):
             for j in range(1, n):
-                grid[i][j] = grid[i - 1][j] + grid[i][j - 1]
-        return grid[-1][-1]
+                current_row[j] = current_row[j - 1] + upper_row[j]
+            upper_row, current_row = current_row, [1] * n
+        return upper_row[-1]
