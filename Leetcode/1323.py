490e2d3d17ab075b9ac53c6114b4be1addbd8441
bishwajit <arin17bishwa@gmail.com>
2025-08-17 11:54:28 +0530

    https://leetcode.com/problems/maximum-69-number/submissions/1738180105

--- Leetcode/1323.py
+++ Leetcode/1323.py
@@ -1,7 +1,3 @@
 class Solution:
     def maximum69Number(self, num: int) -> int:
-        for i in range(5, -1, -1):
-            if (num // pow(10, i)) % 10 == 6:
-                num += 3 * (pow(10, i))
-                break
-        return num
+        return int(str(num).replace("6", "9", 1))
