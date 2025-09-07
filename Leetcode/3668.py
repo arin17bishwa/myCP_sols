e521096a53dfbd997b1d4da33b7c97844895fa97
bishwajit <arin17bishwa@gmail.com>
2025-09-07 15:31:36 +0530

    https://leetcode.com/problems/restore-finishing-order/submissions/1762513304

--- Leetcode/3668.py
+++ Leetcode/3668.py
@@ -3,9 +3,4 @@ from typing import List
 
 class Solution:
     def recoverOrder(self, order: List[int], friends: List[int]) -> List[int]:
-        friends = set(friends)
-        ans = []
-        for i in order:
-            if i in friends:
-                ans.append(i)
-        return ans
+        return [i for i in order if i in set(friends)]
