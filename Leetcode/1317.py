5ce2e2625cd2e1405bc3006b4e84f30d2fe3427f
bishwajit <arin17bishwa@gmail.com>
2025-09-08 22:08:24 +0530

    https://leetcode.com/problems/convert-integer-to-the-sum-of-two-no-zero-integers/submissions/1764017997

--- Leetcode/1317.py
+++ Leetcode/1317.py
@@ -4,11 +4,7 @@ from typing import List
 class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
         def is_valid(x: int) -> bool:
-            while x:
-                if x % 10 == 0:
-                    return False
-                x //= 10
-            return True
+            return "0" not in str(x)
 
         for i in range(1, n + 1):
             if is_valid(i) and is_valid(n - i):
