9c83ccc15270fdca3a2a90d8aaea79335c8a7a7d
bishwajit <arin17bishwa@gmail.com>
2025-09-14 20:36:39 +0530

    https://leetcode.com/problems/vowel-spellchecker/submissions/1770628778

--- Leetcode/966.py
+++ Leetcode/966.py
@@ -4,8 +4,8 @@ from typing import List
 class Solution:
     def spellchecker(self, wordlist: List[str], queries: List[str]) -> List[str]:
 
-        def de_vowel_word(word: str) -> str:
-            intermediate = list(word)
+        def de_vowel_word(key_word: str) -> str:
+            intermediate = list(key_word)
             for idx, ch in enumerate(intermediate):
                 if ch in "aeiou":
                     intermediate[idx] = "_"
@@ -13,7 +13,6 @@ class Solution:
 
         exact_matches: set[str] = set(wordlist)
         case_insensitive_matches: dict[str:str] = {}
-
         vowel_insensitive_matches: dict[str, str] = {}
 
         for word in wordlist:
@@ -24,10 +23,6 @@ class Solution:
             key = de_vowel_word(word.lower())
             if key not in vowel_insensitive_matches:
                 vowel_insensitive_matches[key] = word
-
-        # print(exact_matches)
-        # print(case_insensitive_matches)
-        # print(exact_matches)
 
         def find_match(qword: str) -> str:
             nonlocal exact_matches, case_insensitive_matches, vowel_insensitive_matches
@@ -40,18 +35,3 @@ class Solution:
             return ""
 
         return [find_match(q) for q in queries]
-
-
-def main():
-    obj = Solution()
-    words = ["KiTe", "kite", "hare", "Hare"]
-    q = ["kite", "Kite", "KiTe", "Hare", "HARE", "Hear", "hear", "keti", "keet", "keto"]
-
-    ans = obj.spellchecker(words, q)
-
-    print(q)
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
