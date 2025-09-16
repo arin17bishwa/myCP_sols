9b89770b93b82c91dfb17cb1551279ddd4c8f2be
bishwajit <arin17bishwa@gmail.com>
2025-09-17 01:49:30 +0530

    AC

---
+++ GFG/decode-the-string2444.py
@@ -0,0 +1,23 @@
+class Solution:
+    def decodedString(self, s):
+        # code here
+        stack = []
+        curstr = ""
+        curdig = ""
+        prevdig = ""
+        prevstr = ""
+        for i in range(len(s)):
+            if s[i] == "[":
+                stack.append(curstr)
+                stack.append(curdig)
+                curdig = ""
+                curstr = ""
+            elif s[i] == "]":
+                prevdig = stack.pop()
+                prevstr = stack.pop()
+                curstr = prevstr + curstr * int(prevdig)
+            elif s[i].isdigit():
+                curdig += s[i]
+            else:
+                curstr += s[i]
+        return curstr
