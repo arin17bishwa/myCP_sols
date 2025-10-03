38587470e423db8e3a1ef672b27e867384e31d1d
bishwajit <arin17bishwa@gmail.com>
2025-10-03 15:05:40 +0530

    AC

--- GFG/largest-subarray-with-0-sum.py
+++ GFG/largest-subarray-with-0-sum.py
@@ -11,19 +11,3 @@ class Solution:
             else:
                 ans = max(ans, idx - first_occ[ele])
         return ans
-
-
-def main():
-    obj = Solution()
-
-    arr = [15, -2, 2, -8, 1, 7, 10, 23]
-    arr = [2, 10, 4]
-    arr = [1, 0, -4, 3, 1, 0]
-
-    ans = obj.maxLength(arr)
-
-    # print(ans)
-
-
-if __name__ == "__main__":
-    main()
