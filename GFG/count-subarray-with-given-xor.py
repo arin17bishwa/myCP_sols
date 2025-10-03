d23c5610450923b88e5350138b5bd72985aaa8a4
bishwajit <arin17bishwa@gmail.com>
2025-10-03 15:15:36 +0530

    AC

--- GFG/count-subarray-with-given-xor.py
+++ GFG/count-subarray-with-given-xor.py
@@ -16,22 +16,3 @@ class Solution:
             ans += seen[ele ^ k]
             seen[ele] += 1
         return ans
-
-
-def main():
-    obj = Solution()
-
-    arr = [4, 2, 2, 6, 4]
-    k = 6
-    arr = [5, 6, 7, 8, 9]
-    k = 5
-    arr = [1, 1, 1, 1]
-    k = 0
-
-    ans = obj.subarrayXor(arr, k)
-
-    # print(ans)
-
-
-if __name__ == "__main__":
-    main()
