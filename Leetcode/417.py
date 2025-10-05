0922838eeb529fcf2029a547af9eb0f74cc975e6
bishwajit <arin17bishwa@gmail.com>
2025-10-05 12:14:57 +0530

    https://leetcode.com/problems/pacific-atlantic-water-flow/submissions/1791881124

--- Leetcode/417.py
+++ Leetcode/417.py
@@ -13,32 +13,26 @@ class Solution:
                 if (0 <= x + dx < m) and (0 <= y + dy < n):
                     yield x + dx, y + dy
 
-        def bfs(initial_borders: list[list[int]]) -> list[list[int]]:
+        def bfs(initial_borders: list[list[int]]) -> set[tuple[int, int]]:
             vis = [[0] * n for _ in range(m)]
             for i, j in initial_borders:
                 vis[i][j] = 1
 
             q = deque(initial_borders)
-
+            final_nodes: set[tuple[int, int]] = set(
+                ((i, j) for i, j in initial_borders)
+            )
             while q:
                 x, y = q.popleft()
                 vis[x][y] = 1
                 for _x, _y in find_neighbours(x, y):
                     if (not vis[_x][_y]) and (heights[_x][_y] >= heights[x][y]):
                         q.append([_x, _y])
-            return vis
+                        final_nodes.add((_x, _y))
+            return final_nodes
 
         pacific_borders = [[0, j] for j in range(n)] + [[i, 0] for i in range(1, m)]
         atlantic_borders = [[m - 1, j] for j in range(n)] + [
             [i, n - 1] for i in range(m - 1)
         ]
-
-        vis_pacific = bfs(pacific_borders)
-        vis_atlantic = bfs(atlantic_borders)
-
-        ans: list[list[int]] = []
-        for i in range(m):
-            for j in range(n):
-                if vis_pacific[i][j] and vis_atlantic[i][j]:
-                    ans.append([i, j])
-        return ans
+        return [list(i) for i in bfs(pacific_borders) & bfs(atlantic_borders)]
