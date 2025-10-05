ab32e267a6fc51940661e6e3be63f05913d2bad6
bishwajit <arin17bishwa@gmail.com>
2025-10-05 12:04:34 +0530

    https://leetcode.com/problems/pacific-atlantic-water-flow/submissions/1791872439

--- Leetcode/417.py
+++ Leetcode/417.py
@@ -44,25 +44,3 @@ class Solution:
                 if vis_pacific[i][j] and vis_atlantic[i][j]:
                     ans.append([i, j])
         return ans
-
-
-def main():
-    obj = Solution()
-
-    arr = [
-        [1, 2, 2, 3, 5],
-        [3, 2, 3, 4, 4],
-        [2, 4, 5, 3, 1],
-        [6, 7, 1, 4, 5],
-        [5, 1, 1, 2, 4],
-    ]
-
-    arr = [[1]]
-
-    ans = obj.pacificAtlantic(arr)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
