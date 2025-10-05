686e2850daed3ac9491149bfdbb00dfc729f6203
bishwajit <arin17bishwa@gmail.com>
2025-10-05 12:07:28 +0530

    https://leetcode.com/problems/pacific-atlantic-water-flow/submissions/1791875034

--- Leetcode/417.py
+++ Leetcode/417.py
@@ -1,19 +1,17 @@
 from collections import deque
-from typing import List
+from typing import List, Any, Generator
 
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         m, n = len(heights), len(heights[0])
 
-        def find_neighbours(x: int, y: int) -> list[list[int]]:
-            neighbours = []
+        def find_neighbours(x: int, y: int) -> Generator[tuple[int, int], Any, None]:
             directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
             for dx, dy in directions:
                 if (0 <= x + dx < m) and (0 <= y + dy < n):
-                    neighbours.append([x + dx, y + dy])
-            return neighbours
+                    yield x + dx, y + dy
 
         def bfs(initial_borders: list[list[int]]) -> list[list[int]]:
             vis = [[0] * n for _ in range(m)]
