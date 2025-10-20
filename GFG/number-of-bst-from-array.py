33aeb0826a92f4031246aca3789d62ee456d7d66
bishwajit <arin17bishwa@gmail.com>
2025-10-20 12:55:40 +0530

    AC

--- GFG/number-of-bst-from-array.py
+++ GFG/number-of-bst-from-array.py
@@ -13,18 +13,3 @@ class Solution:
     @cache
     def catalan_number(self, i: int) -> int:
         return factorial(2 * i) // (factorial(i) * factorial(i + 1))
-
-
-def main():
-    obj = Solution()
-
-    arr = [2, 1, 3]
-    arr = [13, 6, 15, 3, 2]
-
-    # ans = obj.countBSTs(arr)
-    # print(ans)
-
-
-if __name__ == "__main__":
-
-    main()
