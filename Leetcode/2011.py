c3b307ee23c62217b7b9892d4f3d3d15c51b22cd
bishwajit <arin17bishwa@gmail.com>
2025-10-20 12:59:08 +0530

    https://leetcode.com/problems/final-value-of-variable-after-performing-operations/submissions/1806538625

---
+++ Leetcode/2011.py
@@ -0,0 +1,9 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        ans = 0
+        for op in operations:
+            if "+" in op:
+                ans += 1
+            else:
+                ans -= 1
+        return ans
