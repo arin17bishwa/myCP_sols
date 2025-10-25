a16c3e12ed6cdc17c350f12a8691a2ea4132efea
bishwajit <arin17bishwa@gmail.com>
2025-10-25 15:36:00 +0530

    https://leetcode.com/problems/calculate-money-in-leetcode-bank/submissions/1811071963

--- Leetcode/1716.py
+++ Leetcode/1716.py
@@ -1,10 +1,8 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        if n <= 7:
-            return sum(list(range(1, 8))[:n])
         k, mod = divmod(n, 7)
         ans = 28 * k + 7 * ((k * (k - 1)) // 2)
-        return ans + ((k - 0) * mod + self.totalMoney(mod))
+        return ans + k * mod + ((mod * (mod + 1)) >> 1)
 
 
 def main():
