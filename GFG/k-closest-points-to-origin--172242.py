e107d8d59c4c5a85180170617de4548c1dbb9ee2
bishwajit <arin17bishwa@gmail.com>
2025-10-25 13:47:23 +0530

    AC

--- GFG/k-closest-points-to-origin--172242.py
+++ GFG/k-closest-points-to-origin--172242.py
@@ -1,3 +1,12 @@
+import heapq
+
+
 class Solution:
     def kClosest(self, points: list[list[int]], k: int) -> list[list[int]]:
-        return sorted(points, key=lambda a: pow(a[0], 2) + pow(a[1], 2))[:k]
+        heap: list[list[int]] = [[i * i + j * j, i, j] for i, j in points]
+        heapq.heapify(heap)
+        ans: list[list[int]] = []
+        for _ in range(k):
+            _, i, j = heapq.heappop(heap)
+            ans.append([i, j])
+        return ans
