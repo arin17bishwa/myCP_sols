3f5161ad499a203f3065412a8347207e4bc53eb7
bishwajit <arin17bishwa@gmail.com>
2025-10-28 19:36:31 +0530

    https://leetcode.com/problems/make-array-elements-equal-to-zero/submissions/1814101629

---
+++ Leetcode/3354.py
@@ -0,0 +1,50 @@
+from typing import List
+
+
+class Solution:
+    def countValidSelections(self, nums: List[int]) -> int:
+        arr = nums
+        n = len(arr)
+        if n == 1:
+            return int(arr[0] == 0) * 2
+
+        prefix = arr[:]
+        suffix = arr[:]
+
+        for i in range(1, n):
+            prefix[i] += prefix[i - 1]
+
+        for i in range(n - 2, -1, -1):
+            suffix[i] += suffix[i + 1]
+
+        ans: int = 0
+        if arr[0] == 0 and suffix[0] <= 1:
+            ans += 1 if suffix[0] == 1 else 2
+        if arr[-1] == 0 and prefix[-1] <= 1:
+            ans += 1 if prefix[-1] == 1 else 2
+
+        for i in range(1, n - 1):
+            if arr[i] == 0:
+                if abs(prefix[i - 1] - suffix[i + 1]) == 1:
+                    ans += 1
+                elif abs(prefix[i - 1] - suffix[i + 1]) == 0:
+                    ans += 2
+
+        return ans
+
+
+def main():
+    obj = Solution()
+    arr = [1, 0, 2, 0, 3]
+    arr = [2, 3, 4, 0, 4, 1, 0]
+    arr = [16, 13, 10, 0, 0, 0, 10, 6, 7, 8, 7]
+    arr = [0, 1]
+    arr = [0, 0, 0, 0, 0, 0, 0, 0]
+
+    ans = obj.countValidSelections(arr)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
