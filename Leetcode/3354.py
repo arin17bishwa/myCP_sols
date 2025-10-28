9945da2c323ad78180ee9bfe8418d8885a423bab
bishwajit <arin17bishwa@gmail.com>
2025-10-28 19:42:30 +0530

    https://leetcode.com/problems/make-array-elements-equal-to-zero/submissions/1814107097

--- Leetcode/3354.py
+++ Leetcode/3354.py
@@ -10,25 +10,21 @@ class Solution:
             return int(arr[0] == 0) * 2
 
         prefix = arr[:]
-        suffix = arr[:]
-
+        tot = sum(arr)
         for i in range(1, n):
             prefix[i] += prefix[i - 1]
 
-        for i in range(n - 2, -1, -1):
-            suffix[i] += suffix[i + 1]
-
         ans: int = 0
-        if arr[0] == 0 and suffix[0] <= 1:
-            ans += 1 if suffix[0] == 1 else 2
+        if arr[0] == 0 and tot <= 1:
+            ans += 1 if tot == 1 else 2
         if arr[-1] == 0 and prefix[-1] <= 1:
             ans += 1 if prefix[-1] == 1 else 2
 
         for i in range(1, n - 1):
             if arr[i] == 0:
-                if abs(prefix[i - 1] - suffix[i + 1]) == 1:
+                if abs(prefix[i - 1] - (tot - prefix[i - 1])) == 1:
                     ans += 1
-                elif abs(prefix[i - 1] - suffix[i + 1]) == 0:
+                elif abs(prefix[i - 1] - (tot - prefix[i - 1])) == 0:
                     ans += 2
 
         return ans
