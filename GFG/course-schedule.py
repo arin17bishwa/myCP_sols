994452225ed849e285ac620a07f54291c9398de7
bishwajit <arin17bishwa@gmail.com>
2025-11-01 21:05:20 +0530

    AC

--- GFG/course-schedule.py
+++ GFG/course-schedule.py
@@ -1,6 +1,7 @@
 class Solution:
     def findOrder(self, n: int, prerequisites: list[list[int]]):
         g: dict[int, list[int]] = {i: [] for i in range(n)}
+
         for i, j in prerequisites:
             g[j].append(i)
 
@@ -22,20 +23,3 @@ class Solution:
                 dfs(k)
 
         return st[::-1]
-
-
-def main():
-    obj = Solution()
-    n = 3
-    arr = [
-        [1, 0],
-        [2, 1],
-    ]
-
-    ans = obj.findOrder(n, arr)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
