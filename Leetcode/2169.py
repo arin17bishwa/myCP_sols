370401d6d749162100d651fc8a8b71da53d509a7
bishwajit <arin17bishwa@gmail.com>
2025-11-09 22:31:28 +0530

    https://leetcode.com/problems/count-operations-to-obtain-zero/submissions/1825288014

--- Leetcode/2169.py
+++ Leetcode/2169.py
@@ -1,6 +1,5 @@
 class Solution:
     def countOperations(self, num1: int, num2: int) -> int:
-        num1, num2 = sorted((num1, num2))
         ans = 0
         while num1 and num2:
             ans += num2 // num1
