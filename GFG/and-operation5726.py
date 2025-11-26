a5428e305cd4e215a314b471297bc50270d30061
bishwajit <arin17bishwa@gmail.com>
2025-11-27 00:02:18 +0530

    AC

--- GFG/and-operation5726.py
+++ GFG/and-operation5726.py
@@ -1,25 +1,9 @@
 class Solution:
     def andInRange(self, l: int, r: int):
-        shift=0
+        shift: int = 0
 
-        while l!=r:
-            l>>=1
-            r>>=1
-            shift+=1
-        return l<<shift
-
-
-def main():
-    obj = Solution()
-
-    l, r = 8, 13
-    # l, r = 2, 3
-    # l,r=4,5
-
-    ans = obj.andInRange(l, r)
-
-    # print(ans)
-
-
-if __name__ == "__main__":
-    main()
+        while l != r:
+            l >>= 1
+            r >>= 1
+            shift += 1
+        return l << shift
