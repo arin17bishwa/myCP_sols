54ca51c45e72f6708a803eb6c5ae332ae11020e1
bishwajit <arin17bishwa@gmail.com>
2025-12-04 00:00:50 +0530

    https://leetcode.com/problems/shuffle-the-array/submissions/1846184975

---
+++ Leetcode/1470.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        ans = []
+        for i in range(n):
+            ans.extend([nums[i], nums[i + n]])
+        return ans
