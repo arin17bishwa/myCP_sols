8061ff295b37609123de1b3a0fc1070e317cae8e
bishwajit <arin17bishwa@gmail.com>
2025-12-08 21:55:10 +0530

    https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/submissions/1850320156

--- Leetcode/1523.py
+++ Leetcode/1523.py
@@ -1,19 +1,3 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
         return ((high - low) >> 1) + ((low | high) & 1)
-
-
-def main():
-    obj = Solution()
-    low, high = 3, 7
-    low, high = 8, 10
-    low, high = 4, 7
-    low, high = 3, 8
-    low, high = 11, 13
-
-    ans = obj.countOdds(low, high)
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
