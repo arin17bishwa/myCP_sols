525e8b5d6245ba02eefa2b6a9df2a24dd918c3b3
bishwajit <arin17bishwa@gmail.com>
2025-12-08 21:39:06 +0530

    https://leetcode.com/problems/count-square-sum-triples/submissions/1850305060

--- Leetcode/1925.py
+++ Leetcode/1925.py
@@ -2,9 +2,9 @@ class Solution:
     def countTriples(self, n: int) -> int:
         ans = 0
         for a in range(1, n + 1):
-            for b in range(1, n + 1):
+            for b in range(a, n + 1):
                 c2 = a * a + b * b
                 c = int(pow(c2, 0.5))
                 if c <= n and abs(c * c - c2) < 1e-6:
-                    ans += 1
+                    ans += 2
         return ans
