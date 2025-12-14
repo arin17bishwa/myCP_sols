9e9f706468051a64abb22645f2efa1956add0c35
bishwajit <arin17bishwa@gmail.com>
2025-12-14 09:36:49 +0530

    AC

--- GFG/2d-submatrix-sum-queries.py
+++ GFG/2d-submatrix-sum-queries.py
@@ -1,6 +1,7 @@
 class Solution:
     def prefixSum2D(self, mat: list[list[int]], queries: list[int]) -> list[int]:
         m, n = len(mat), len(mat[0])
+
         for j in range(1, n):
             mat[0][j] += mat[0][j - 1]
         for i in range(1, m):
@@ -9,13 +10,13 @@ class Solution:
         for i in range(1, m):
             for j in range(1, n):
                 mat[i][j] += mat[i - 1][j] + mat[i][j - 1] - mat[i - 1][j - 1]
+
         ans = []
         for r1, c1, r2, c2 in queries:
-            tot = (
+            ans.append(
                 mat[r2][c2]
                 - (mat[r2][c1 - 1] * (c1 != 0))
                 - (mat[r1 - 1][c2] * (r1 != 0))
                 + (mat[r1 - 1][c1 - 1] * (r1 != 0 and c1 != 0))
             )
-            ans.append(tot)
         return ans
