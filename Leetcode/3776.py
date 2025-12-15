379f32491f5e82b41d9605dc0652662eddf9adb1
bishwajit <arin17bishwa@gmail.com>
2025-12-15 20:13:26 +0530

    https://leetcode.com/problems/minimum-moves-to-balance-circular-array/submissions/1856320885

--- Leetcode/3776.py
+++ Leetcode/3776.py
@@ -6,17 +6,21 @@ class Solution:
         arr = balance
         n = len(arr)
         tot = sum(arr)
+
         if tot < 0:
             return -1
         ans = need = 0
         neg = -1
+
         for idx, i in enumerate(arr):
             if i < 0:
                 neg = idx
                 need = -i
                 break
+
         if neg == -1:
             return 0
+
         balances = [
             (min((neg - i) % n, (i - neg) % n), ele)
             for i, ele in enumerate(arr)
@@ -31,4 +35,5 @@ class Solution:
             need -= take
             if need == 0:
                 break
+
         return ans
