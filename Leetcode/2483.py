5b70e24226b5e5d19e3c00508bf85a1b5377b8dc
bishwajit <arin17bishwa@gmail.com>
2025-12-27 00:20:24 +0530

    https://leetcode.com/problems/minimum-penalty-for-a-shop/submissions/1866089434

--- Leetcode/2483.py
+++ Leetcode/2483.py
@@ -2,21 +2,18 @@ class Solution:
     def bestClosingTime(self, customers: str) -> int:
         n: int = len(customers)
         arr: list[int] = [int(i == "Y") for i in customers]
+        tot = sum(arr)
+        min_penalty = tot
+        ans = 0
+
         for i in range(1, n):
             arr[i] += arr[i - 1]
-
-        if arr[-1] == 0:
-            return 0
-
-        min_penalty = arr[-1]
-        ans = 0
-        for i in range(1, n + 1):
-            curr_penalty = (i - arr[i - 1]) + (arr[-1] - arr[i - 1])
+            curr_penalty = (i - arr[i - 1]) + (tot - arr[i - 1])
             if curr_penalty < min_penalty:
                 min_penalty = curr_penalty
                 ans = i
 
-        return ans
+        return ans if min_penalty <= (n - tot) else n
 
 
 def func():
@@ -24,7 +21,7 @@ def func():
 
     s = "YYNY"
 
-    s="NNNYNN"
+    s = "NNNYNN"
 
     ans = obj.bestClosingTime(s)
 
