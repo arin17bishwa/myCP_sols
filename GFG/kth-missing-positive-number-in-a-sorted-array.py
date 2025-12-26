898d2c55737ab9db26b281ad1b754b406e385188
bishwajit <arin17bishwa@gmail.com>
2025-12-26 10:35:01 +0530

    AC

--- GFG/kth-missing-positive-number-in-a-sorted-array.py
+++ GFG/kth-missing-positive-number-in-a-sorted-array.py
@@ -13,27 +13,3 @@ class Solution:
                 lo = mid + 1
 
         return n + k if ans == -1 else ans
-
-
-def func():
-    obj = Solution()
-
-    arr = [2, 3, 4, 7, 11]
-    k = 5
-
-    arr = [1, 2, 3]
-    k = 2
-
-    arr = [3, 5, 9, 10, 11, 12]
-    k = 2
-
-    ans = obj.kthMissing(arr, k)
-    # print(ans)
-
-
-def main():
-    func()
-
-
-if __name__ == "__main__":
-    main()
