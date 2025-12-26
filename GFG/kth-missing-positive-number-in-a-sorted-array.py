6fc6a67e2659d9aa065c39ab3401b624ade413cd
bishwajit <arin17bishwa@gmail.com>
2025-12-26 10:29:20 +0530

    AC

--- GFG/kth-missing-positive-number-in-a-sorted-array.py
+++ GFG/kth-missing-positive-number-in-a-sorted-array.py
@@ -1,10 +1,18 @@
 class Solution:
     def kthMissing(self, arr: list[int], k: int) -> int:
         n = len(arr)
-        for i in range(n):
-            if arr[i] > i + k:
-                return i + k
-        return n + k
+        lo, hi = 0, n - 1
+        ans = -1
+
+        while lo <= hi:
+            mid = (lo + hi) >> 1
+            if arr[mid] > mid + k:
+                ans = mid + k
+                hi = mid - 1
+            else:
+                lo = mid + 1
+
+        return n + k if ans == -1 else ans
 
 
 def func():
