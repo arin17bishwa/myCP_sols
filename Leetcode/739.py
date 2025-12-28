ffad9a7a332510e1660312ac1bdc76c89c68dcb4
bishwajit <arin17bishwa@gmail.com>
2025-12-28 05:49:34 +0530

    https://leetcode.com/problems/daily-temperatures/submissions/1867042798

--- Leetcode/739.py
+++ Leetcode/739.py
@@ -15,20 +15,3 @@ class Solution:
                 ans[i] = q[0] - i
             q.appendleft(i)
         return ans
-
-
-def main():
-    obj = Solution()
-
-    arr = [73, 74, 75, 71, 69, 72, 76, 73]
-    arr = [30, 40, 50, 60]
-    arr = [30, 60, 90]
-    arr = [89, 62, 70, 58, 47, 47, 46, 76, 100, 70]
-
-    ans = obj.dailyTemperatures(arr)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
