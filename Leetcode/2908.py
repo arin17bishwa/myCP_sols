3ba5b93a155dc0cde579d1cf310890aab55af6f1
bishwajit <arin17bishwa@gmail.com>
2025-12-31 14:03:07 +0530

    https://leetcode.com/problems/minimum-sum-of-mountain-triplets-i/submissions/1870103781

--- Leetcode/2908.py
+++ Leetcode/2908.py
@@ -6,11 +6,35 @@ class Solution:
         arr = nums
         n = len(arr)
         int_max = (1 << 32) - 1
-        ans = int_max
+        prefix_min = [int_max] * n
+        suffix_min = [int_max] * n
 
-        for i in range(n - 2):
-            for j in range(i + 1, n - 1):
-                for k in range(j + 1, n):
-                    if arr[i] < arr[j] and arr[j] > arr[k]:
-                        ans = min(ans, arr[i] + arr[j] + arr[k])
-        return ans if ans != int_max else -1
+        for i in range(1, n):
+            prefix_min[i] = min(prefix_min[i - 1], arr[i - 1])
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(suffix_min[i + 1], arr[i + 1])
+
+        # print(arr)
+        # print(prefix_min)
+        # print(suffix_min)
+
+        ans = int_max
+        for i in range(1, n - 1):
+            if prefix_min[i] < arr[i] and suffix_min[i] < arr[i]:
+                ans = min(ans, prefix_min[i] + arr[i] + suffix_min[i])
+        return ans if not ans >= int_max else -1
+
+
+def main():
+    obj = Solution()
+
+    arr = [5, 4, 8, 7, 10, 2]
+
+    ans = obj.minimumSum(arr)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
