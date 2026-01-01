7070db53c8c460431a2691fce2088dad06b54d03
bishwajit <arin17bishwa@gmail.com>
2026-01-01 23:39:15 +0530

    AC

--- GFG/second-largest3735.py
+++ GFG/second-largest3735.py
@@ -1,19 +1,22 @@
 class Solution:
     def getSecondLargest(self, arr: list[int]) -> int:
-        mx = max(arr)
-        ans = -1
+        largest = second_largest = arr[0]
 
         for ele in arr:
-            if ele < mx:
-                ans = max(ans, ele)
-        return ans
+            if largest == second_largest and ele != largest:
+                largest, second_largest = max(largest, ele), min(largest, ele)
+            elif ele > largest:
+                largest, second_largest = ele, largest
+            elif largest > ele > second_largest:
+                second_largest = ele
+        return -1 if largest == second_largest else second_largest
 
 
 def main():
     obj = Solution()
 
     arr = [10, 5, 10]
-    arr = [10, 10, 1]
+    # arr = [10, 10, 1]
 
     ans = obj.getSecondLargest(arr)
 
