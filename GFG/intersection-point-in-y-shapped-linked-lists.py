ea597fdb36b7c6e7a1a0267fd337deeac774469d
bishwajit <arin17bishwa@gmail.com>
2026-01-01 22:27:33 +0530

    AC

--- GFG/intersection-point-in-y-shapped-linked-lists.py
+++ GFG/intersection-point-in-y-shapped-linked-lists.py
@@ -4,18 +4,29 @@ class Node:
         self.next = None
 
 
+def len_ll(head: Node) -> int:
+    curr = head
+    ans = 0
+    while curr:
+        ans += 1
+        curr = curr.next
+    return ans
+
+
 class Solution:
     def intersectPoint(self, head1: Node, head2: Node) -> Node:
-        seen: set[Node] = set()
+        l1 = len_ll(head1)
+        l2 = len_ll(head2)
 
-        curr = head1
-        while curr:
-            seen.add(curr)
-            curr = curr.next
+        if l1 <= l2:
+            for _ in range(l2 - l1):
+                head2 = head2.next
+        else:
+            for _ in range(l1 - l2):
+                head1 = head1.next
 
-        curr = head2
+        while head1 != head2:
+            head1 = head1.next
+            head2 = head2.next
 
-        while curr not in seen:
-            curr = curr.next
-
-        return curr
+        return head1
