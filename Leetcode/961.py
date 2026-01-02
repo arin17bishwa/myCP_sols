dc09eb8c32aff356c5d3d02401f8a96194a94060
bishwajit <arin17bishwa@gmail.com>
2026-01-02 12:50:08 +0530

    https://leetcode.com/problems/n-repeated-element-in-size-2n-array/submissions/1871757704

--- Leetcode/961.py
+++ Leetcode/961.py
@@ -1,11 +1,12 @@
-from collections import Counter
 from typing import List
 
 
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        n = len(nums) >> 1
-        for k, v in Counter(nums).items():
-            if v == n:
-                return k
+        arr = nums
+        m = len(arr)
+
+        for i in range(m):
+            if arr[i] in arr[i + 1 : i + 4]:
+                return arr[i]
         return -1
