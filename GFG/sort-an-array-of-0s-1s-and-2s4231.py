7a70eb56ac1b11e3f35308c5485457e0b2485d2e
bishwajit <arin17bishwa@gmail.com>
2026-01-02 12:40:00 +0530

    AC

--- GFG/sort-an-array-of-0s-1s-and-2s4231.py
+++ GFG/sort-an-array-of-0s-1s-and-2s4231.py
@@ -1,18 +1,25 @@
-from collections import Counter
-
-
 class Solution:
     def sort012(self, arr: list[int]) -> None:
-        freq = Counter(arr)
-        arr[: freq[0]] = [0] * freq[0]
-        arr[freq[0] : freq[0] + freq[1]] = [1] * freq[1]
-        arr[freq[0] + freq[1] :] = [2] * freq[2]
+        n = len(arr)
+        head, tail = 0, n - 1
+        curr = 0
+
+        while curr <= tail:
+            if arr[curr] == 0:
+                arr[curr], arr[head] = arr[head], arr[curr]
+                curr += 1
+                head += 1
+            elif arr[curr] == 2:
+                arr[curr], arr[tail] = arr[tail], arr[curr]
+                tail -= 1
+            else:
+                curr += 1
 
 
 def main():
     obj = Solution()
 
-    arr = [0, 1, 1]
+    arr = [2, 2, 2, 1, 0, 1, 0, 1, 1]
 
     obj.sort012(arr)
 
