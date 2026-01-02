1bc532cc8e5870924f5ae3901a373780fe6bd784
bishwajit <arin17bishwa@gmail.com>
2026-01-02 17:09:38 +0530

    https://leetcode.com/problems/product-of-array-except-self/submissions/1871974408

--- Leetcode/238.py
+++ Leetcode/238.py
@@ -6,26 +6,15 @@ class Solution:
         arr = nums
         n = len(arr)
         ans = arr[:]
+        curr = arr[-1]
+
         for i in range(1, n - 1):
             ans[i] *= ans[i - 1]
         ans[-1] = ans[-2]
-        curr = arr[-1]
+
         for i in range(n - 2, 0, -1):
             ans[i] = ans[i - 1] * curr
             curr *= arr[i]
+
         ans[0] = curr
         return ans
-
-
-def main():
-    obj = Solution()
-
-    arr = [1, 2, 3, 4]
-
-    ans = obj.productExceptSelf(arr)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
