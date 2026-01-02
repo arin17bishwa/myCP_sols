95d526e2097e628d49b9259fe1067ff2d5dd0d09
bishwajit <arin17bishwa@gmail.com>
2026-01-02 20:13:16 +0530

    AC

--- GFG/find-nth-root-of-m5843.py
+++ GFG/find-nth-root-of-m5843.py
@@ -13,19 +13,3 @@ class Solution:
             else:
                 hi = mid - 1
         return -1
-
-
-def main():
-    obj = Solution()
-    n, m = 3, 8
-    n, m = 3, 9
-    n, m = 4, 16
-    n, m = 6, 0
-
-    ans = obj.nthRoot(n, m)
-
-    # print(ans)
-
-
-if __name__ == "__main__":
-    main()
