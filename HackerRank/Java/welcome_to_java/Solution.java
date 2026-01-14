ef5356dce007436092f9a21dd1e7ee5939fa4b49
bishwajit <arin17bishwa@gmail.com>
2026-01-15 01:51:33 +0530

    https://www.hackerrank.com/challenges/welcome-to-java/submissions/code/460038938

---
+++ HackerRank/Java/welcome_to_java/Solution.java
@@ -0,0 +1,10 @@
+package Java.welcome_to_java;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
