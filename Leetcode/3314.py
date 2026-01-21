037792339c6c6ca155bae7638409119c6af9f4e3
bishwajit <arin17bishwa@gmail.com>
2026-01-21 23:58:22 +0530

    https://leetcode.com/problems/construct-the-minimum-bitwise-array-i/submissions/1891307559

--- Leetcode/3314.py
+++ Leetcode/3314.py
@@ -14,18 +14,3 @@ class Solution:
             return res
 
         return [func(i) for i in arr]
-
-
-def main():
-    obj = Solution()
-
-    arr = [2, 3, 5, 7]
-    arr = [11, 13, 31]
-
-    ans = obj.minBitwiseArray(arr)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
