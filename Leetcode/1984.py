075cf0d0410384bae00d4d49c86e64d58c6edd98
bishwajit <arin17bishwa@gmail.com>
2026-01-25 09:25:55 +0530

    https://leetcode.com/problems/minimum-difference-between-highest-and-lowest-of-k-scores/submissions/1896035791

--- Leetcode/1984.py
+++ Leetcode/1984.py
@@ -8,18 +8,3 @@ class Solution:
         n = len(arr)
 
         return min(arr[i] - arr[i - k + 1] for i in range(k - 1, n))
-
-
-def main():
-    obj = Solution()
-
-    arr = [9, 4, 1, 7]
-    k = 2
-
-    ans = obj.minimumDifference(arr, k)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
