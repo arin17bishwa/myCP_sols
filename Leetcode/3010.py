142b647ba159a4b11436c40fa9692eae59c476d5
bishwajit <arin17bishwa@gmail.com>
2026-02-02 02:14:02 +0530

    https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/submissions/1904797160

--- Leetcode/3010.py
+++ Leetcode/3010.py
@@ -1,7 +1,10 @@
+import heapq
 from typing import List
 
 
 class Solution:
     def minimumCost(self, nums: List[int]) -> int:
         arr = nums
-        return arr[0] + sum(sorted(arr[1:])[:2])
+        heap = arr[1:]
+        heapq.heapify(heap)
+        return arr[0] + sum(heapq.nsmallest(2, heap))
