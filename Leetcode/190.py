d19fc4f1650777b76500898b7467ca133ad5ba8f
bishwajit <arin17bishwa@gmail.com>
2026-02-16 22:14:34 +0530

    https://leetcode.com/problems/reverse-bits/submissions/1921264718

--- Leetcode/190.py
+++ Leetcode/190.py
@@ -1,22 +1,8 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         ans = 0
-        for i in range(31):
+        for _ in range(31):
             ans |= n & 1
             n >>= 1
             ans <<= 1
         return ans
-
-
-def main():
-    obj = Solution()
-
-    n = 43261596
-
-    ans = obj.reverseBits(n)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
