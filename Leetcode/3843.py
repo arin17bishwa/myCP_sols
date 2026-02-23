e523abcef73bad4e86a109e56647f37c8e373cd5
bishwajit <arin17bishwa@gmail.com>
2026-02-23 22:34:30 +0530

    https://leetcode.com/problems/first-element-with-unique-frequency/submissions/1928769656

---
+++ Leetcode/3843.py
@@ -0,0 +1,14 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def firstUniqueFreq(self, nums: List[int]) -> int:
+        arr = nums
+        n = len(arr)
+        freq = Counter(arr)
+        freq_freq = Counter(freq.values())
+        for i in arr:
+            if freq_freq[freq[i]] == 1:
+                return i
+        return -1
