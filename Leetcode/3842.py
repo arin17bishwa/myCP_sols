2d30b98b49e1f4332cc849b1eb5a11429363fd32
bishwajit <arin17bishwa@gmail.com>
2026-02-23 22:33:36 +0530

    https://leetcode.com/problems/toggle-light-bulbs/submissions/1928768451

--- Leetcode/3842.py
+++ Leetcode/3842.py
@@ -1,13 +1,9 @@
 class Solution:
     def toggleLightBulbs(self, bulbs: list[int]) -> list[int]:
         arr = bulbs
-        n = len(arr)
         state = [0] * 101
+
         for i in arr:
             state[i] ^= 1
 
-        ans = []
-        for i in range(1, 101):
-            if state[i]:
-                ans.append(i)
-        return ans
+        return [i for i in range(1, 101) if state[i]]
