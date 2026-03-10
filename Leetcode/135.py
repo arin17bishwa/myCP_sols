c2f7110e718b15bf4daf0a2802f5098b0e316e17
Bishwajit Ghosh <b-ghosh@ti.com>
2026-03-10 13:30:56 +0530

    https://leetcode.com/problems/candy/submissions/1943698093

--- Leetcode/135.py
+++ Leetcode/135.py
@@ -4,7 +4,6 @@ class Solution:
     def candy(self, ratings: List[int]) -> int:
         arr=ratings
         n=len(arr)
-
         ans=[1]*n
 
         for i in range(1,n):
@@ -16,18 +15,3 @@ class Solution:
                 ans[i]=max(ans[i], ans[i+1]+1)
 
         return sum(ans)
-
-def main():
-    obj=Solution()
-
-    arr=[1,0,2]
-    arr=[1,2,2]
-    arr=[1,3,2,2,1]
-    arr=[1,2,87,87,87,2,1]
-
-    ans=obj.candy(arr)
-
-    print(ans)
-
-if __name__ == '__main__':
-    main()
