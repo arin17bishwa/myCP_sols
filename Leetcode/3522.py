6b6520bd29073c548de7d80cc0a4af321d1ad463
Bishwajit Ghosh <b-ghosh@ti.com>
2026-03-12 12:33:34 +0530

    https://leetcode.com/problems/calculate-score-after-performing-instructions/submissions/1945766242

---
+++ Leetcode/3522.py
@@ -0,0 +1,38 @@
+from typing import List
+
+class Solution:
+    def calculateScore(self, instructions: List[str], values: List[int]) -> int:
+        arr=instructions
+        n=len(arr)
+        seen:set[int]=set()
+
+        ans=idx=0
+
+        while 0<=idx<n and (idx not in seen ):
+            seen.add(idx)
+            if arr[idx]=='add':
+                ans+=values[idx]
+                idx+=1
+            else:
+                idx=idx+values[idx]
+        return ans
+
+def main():
+    obj=Solution()
+
+    ins=["jump","add","add","jump","add","jump"]
+    val=[2,1,3,1,-2,-3]
+
+    ins=["jump","add","add"]
+    val=[3,1,1]
+
+    ins=['jump']
+    val=[0]
+
+    ans=obj.calculateScore(ins,val)
+
+    print(ans)
+
+if __name__ == '__main__':
+    main()
+
