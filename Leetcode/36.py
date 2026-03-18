629155607b46c1b0424ee153201a23796e7d73ed
bishwajit <arin17bishwa@gmail.com>
2026-03-18 20:24:17 +0530

    https://leetcode.com/problems/valid-sudoku/submissions/1952192266

---
+++ Leetcode/36.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        n = 9
+        rows = [defaultdict(int) for _ in range(n)]
+        cols = [defaultdict(int) for _ in range(n)]
+
+        for i in range(n):
+            for j in range(n):
+                rows[i][board[i][j]] += 1
+                cols[j][board[i][j]] += 1
+                if board[i][j] != "." and (
+                    rows[i][board[i][j]] > 1 or cols[j][board[i][j]] > 1
+                ):
+                    return False
+        for x in range(0, n, 3):
+            for y in range(0, n, 3):
+                seen = set()
+                for i in range(x, x + 3):
+                    for j in range(y, y + 3):
+                        if board[i][j] in seen:
+                            return False
+                        if board[i][j] != ".":
+                            seen.add(board[i][j])
+
+        return True
+
+
+def main():
+    obj = Solution()
+    arr = [
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+    ]
+
+    arr = [
+        ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+    ]
+    ans = obj.isValidSudoku(arr)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
