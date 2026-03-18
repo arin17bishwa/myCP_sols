8685642060c5d6a90fe7f626729b2ae1460d1e7b
bishwajit <arin17bishwa@gmail.com>
2026-03-18 20:32:29 +0530

    https://leetcode.com/problems/longest-consecutive-sequence/submissions/1952199235

--- Leetcode/128.py
+++ Leetcode/128.py
@@ -3,17 +3,14 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        arr = nums
         ans = 0
-        there = set(arr)
+        there = set(nums)
         for i in there:
             if i - 1 in there:
                 continue
             else:
                 x = i
-                curr = 1
                 while x + 1 in there:
                     x += 1
-                    curr += 1
-                ans = max(ans, curr)
+                ans = max(ans, x - i + 1)
         return ans
