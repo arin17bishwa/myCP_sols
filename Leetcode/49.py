200a946506f0d59a727713bf859cdb712d261593
bishwajit <arin17bishwa@gmail.com>
2026-03-26 06:45:23 +0530

    https://leetcode.com/problems/group-anagrams/submissions/1959406652

--- Leetcode/49.py
+++ Leetcode/49.py
@@ -2,16 +2,17 @@ from collections import defaultdict
 from typing import List
 
 
-def make_key(s: str) -> tuple[int, ...]:
-    freq = [0] * 26
-    for ch in s:
-        freq[ord(ch) - 97] += 1
-    return tuple(freq)
-
-
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp: dict[tuple[int, ...], list[str]] = defaultdict(list)
+
+        def calculate_freq(st: str) -> tuple[int, ...]:
+            freq = [0] * 26
+            for ch in st:
+                freq[ord(ch) - 97] += 1
+            return tuple(freq)
+
+        ans: defaultdict[tuple[int, ...], list[str]] = defaultdict(list)
+
         for s in strs:
-            mp[make_key(s)].append(s)
-        return list(mp.values())
+            ans[calculate_freq(s)].append(s)
+        return list(ans.values())
