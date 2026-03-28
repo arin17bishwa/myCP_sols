2beb083aaba5530a0267818b6abc3352b2361dda
bishwajit <arin17bishwa@gmail.com>
2026-03-29 04:11:12 +0530

    https://leetcode.com/problems/remove-element/submissions/1962254417

--- Leetcode/27.py
+++ Leetcode/27.py
@@ -7,16 +7,14 @@ class Solution:
         n = len(arr)
 
         head, tail = 0, n - 1
-        seen = 0
         while head <= tail:
             if arr[head] == val:
                 arr[tail], arr[head] = arr[head], arr[tail]
                 tail -= 1
-                seen += 1
             else:
                 head += 1
 
-        return n - seen
+        return tail + 1
 
 
 def main():
