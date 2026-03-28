de7927a0fe5d04a79a38a8582e7fefceba521f72
bishwajit <arin17bishwa@gmail.com>
2026-03-29 04:10:26 +0530

    https://leetcode.com/problems/remove-element/submissions/1962254153

--- Leetcode/27.py
+++ Leetcode/27.py
@@ -6,14 +6,35 @@ class Solution:
         arr = nums
         n = len(arr)
 
-        curr_idx = pointer = 0
-        while pointer < n:
-            while pointer < n and arr[pointer] == val:
-                pointer += 1
-            if pointer >= n:
-                break
-            arr[curr_idx] = arr[pointer]
-            curr_idx += 1
-            pointer += 1
+        head, tail = 0, n - 1
+        seen = 0
+        while head <= tail:
+            if arr[head] == val:
+                arr[tail], arr[head] = arr[head], arr[tail]
+                tail -= 1
+                seen += 1
+            else:
+                head += 1
 
-        return curr_idx
+        return n - seen
+
+
+def main():
+    obj = Solution()
+
+    arr = [3, 2, 2, 3]
+    k = 3
+
+    # arr = [0, 1, 2, 2, 3, 0, 4, 2]
+    # k = 2
+
+    # arr=[1]
+    # k=1
+
+    ans = obj.removeElement(arr, k)
+
+    print(arr[:ans], arr)
+
+
+if __name__ == "__main__":
+    main()
