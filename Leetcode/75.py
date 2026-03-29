05cc35d28a5b9138669ac47be80cfc3ddeed72bc
bishwajit <arin17bishwa@gmail.com>
2026-03-29 10:01:39 +0530

    https://leetcode.com/problems/sort-colors/submissions/1962482649

--- Leetcode/75.py
+++ Leetcode/75.py
@@ -8,16 +8,15 @@ class Solution:
         """
         arr = nums
         n = len(arr)
-        low = mid = 0
-        high = n - 1
-        while mid <= high:
+        lo, mid, hi = 0, 0, n - 1
+
+        while mid <= hi:
             if arr[mid] == 0:
-                arr[low], arr[mid] = arr[mid], arr[low]
+                arr[lo], arr[mid] = arr[mid], arr[lo]
+                lo += 1
                 mid += 1
-                low += 1
             elif arr[mid] == 1:
                 mid += 1
             else:
-                arr[mid], arr[high] = arr[high], arr[mid]
-                high -= 1
-        return
+                arr[mid], arr[hi] = arr[hi], arr[mid]
+                hi -= 1
