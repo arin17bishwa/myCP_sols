b4f6ac0393f19943038926637dc1b08b455e1128
bishwajit <arin17bishwa@gmail.com>
2026-03-30 21:10:44 +0530

    https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-ii/submissions/1963977375

--- Leetcode/2840.py
+++ Leetcode/2840.py
@@ -1,8 +1,14 @@
-from collections import Counter
-
-
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
-        return Counter(s1[::2]) == Counter(s2[::2]) and Counter(s1[1::2]) == Counter(
-            s2[1::2]
-        )
+        n = len(s1)
+
+        arr1 = [[0] * 26 for _ in range(2)]
+        arr2 = [[0] * 26 for _ in range(2)]
+
+        for idx, ch in enumerate(s1):
+            arr1[(idx & 1) != 0][ord(ch) - 97] += 1
+
+        for idx, ch in enumerate(s2):
+            arr2[(idx & 1) != 0][ord(ch) - 97] += 1
+
+        return arr1 == arr2
