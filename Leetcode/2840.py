b6896b5127ac0efa1395cedf27959023c75c72b8
bishwajit <arin17bishwa@gmail.com>
2026-03-30 21:06:34 +0530

    https://leetcode.com/problems/check-if-strings-can-be-made-equal-with-operations-ii/submissions/1963973313

--- Leetcode/2840.py
+++ Leetcode/2840.py
@@ -3,9 +3,6 @@ from collections import Counter
 
 class Solution:
     def checkStrings(self, s1: str, s2: str) -> bool:
-        s1_even = Counter(s1[::2])
-        s1_odd = Counter(s1[1::2])
-        s2_even = Counter(s2[::2])
-        s2_odd = Counter(s2[1::2])
-
-        return s1_even == s2_even and s1_odd == s2_odd
+        return Counter(s1[::2]) == Counter(s2[::2]) and Counter(s1[1::2]) == Counter(
+            s2[1::2]
+        )
