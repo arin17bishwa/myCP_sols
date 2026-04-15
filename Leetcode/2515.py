21b03f828f4ee992e9f03fc464107ac40df67fb9
arin17bishwa <arin17bishwa@gmail.com>
2026-04-15 16:30:32 +0530

    https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/submissions/1979104214

--- Leetcode/2515.py
+++ Leetcode/2515.py
@@ -4,6 +4,7 @@ from typing import List
 class Solution:
     def closestTarget(self, words: List[str], target: str, startIndex: int) -> int:
         arr = words
+
         n = len(arr)
         ans: int = n + 1
 
@@ -11,27 +12,3 @@ class Solution:
             if arr[i % n] == target:
                 ans = min(ans, i - startIndex, n - (i - startIndex))
         return -1 if ans == n + 1 else ans
-
-
-def main():
-    obj = Solution()
-
-    arr = ["hello", "i", "am", "leetcode", "hello"]
-    s = "hello"
-    x = 1
-
-    arr = ["a", "b", "leetcode"]
-    s = "leetcode"
-    x = 0
-
-    arr = ["i", "eat", "leetcode"]
-    s = "ate"
-    x = 0
-
-    ans = obj.closestTarget(arr, s, x)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
