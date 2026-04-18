8e5800d42479c0702b1d916508ef1eb5d8475ddc
bishwajit <arin17bishwa@gmail.com>
2026-04-18 23:46:55 +0530

    AC

--- GFG/flip-bits0240.py
+++ GFG/flip-bits0240.py
@@ -1,6 +1,5 @@
 class Solution:
     def maxOnes(self, arr: list[int]) -> int:
-        n = len(arr)
         kd = [-1 if i == 1 else 1 for i in arr]
 
         mx = curr = 0
@@ -11,18 +10,3 @@ class Solution:
                 curr = 0
 
         return sum(arr) + mx
-
-
-def main():
-    obj = Solution()
-
-    arr = [1, 0, 0, 1, 0]
-    # arr = [1, 0, 0, 1, 0, 0, 1]
-
-    ans = obj.maxOnes(arr)
-
-    # print(ans)
-
-
-if __name__ == "__main__":
-    main()
