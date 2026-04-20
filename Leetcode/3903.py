b98d8d0a405c7f8c82afb5c45f8fcc1c45fc4bc6
bishwajit <arin17bishwa@gmail.com>
2026-04-21 03:39:02 +0530

    https://leetcode.com/problems/smallest-stable-index-i/submissions/1983983581

--- Leetcode/3903.py
+++ Leetcode/3903.py
@@ -1,15 +1,21 @@
+from collections import deque
+
+
 class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         arr = nums
         n = len(arr)
         mx = arr[0]
-        suffix = arr[:]
+        d: deque[tuple[int, int]] = deque([(n - 1, arr[-1])])
 
         for i in range(n - 2, -1, -1):
-            suffix[i] = min(suffix[i], suffix[i + 1])
+            if arr[i] < d[0][-1]:
+                d.appendleft((i, arr[i]))
 
         for i in range(n):
             mx = max(mx, arr[i])
-            if mx - suffix[i] <= k:
+            if i > d[0][0]:
+                d.popleft()
+            if mx - d[0][1] <= k:
                 return i
         return -1
