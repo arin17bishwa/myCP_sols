4e8593fcbac3b9e5c840b53f77da8fb7b8647fee
bishwajit <arin17bishwa@gmail.com>
2026-05-19 21:41:25 +0530

    https://leetcode.com/problems/check-adjacent-digit-differences/submissions/2007452905

--- Leetcode/3931.py
+++ Leetcode/3931.py
@@ -3,6 +3,6 @@ class Solution:
         n = len(s)
 
         for i in range(1, n):
-            if abs(int(s[i - 1]) - int(s[i])) > 2:
+            if abs(ord(s[i - 1]) - ord(s[i])) > 2:
                 return False
         return True
