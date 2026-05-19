aa074a3895a30223373323168e9f4d55b77b0cf6
bishwajit <arin17bishwa@gmail.com>
2026-05-19 21:35:29 +0530

    https://leetcode.com/problems/minimum-common-value/submissions/2007448702

--- Leetcode/2540.py
+++ Leetcode/2540.py
@@ -3,4 +3,31 @@ from typing import List
 
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        return min(set(nums1).intersection(set(nums2)), default=-1)
+        m, n = len(nums1), len(nums2)
+        ans = -1
+        i = j = 0
+
+        while i < m and j < n:
+            a, b = nums1[i], nums2[j]
+            if a == b:
+                return a
+            elif a < b:
+                i += 1
+            else:
+                j += 1
+        return ans
+
+
+def main():
+    obj = Solution()
+
+    arr1 = [1, 2, 3]
+    arr2 = [2, 4]
+
+    ans = obj.getCommon(arr1, arr2)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
