9d752f1b1803d09fcba48ed2d1825e31c32384d7
bishwajit <arin17bishwa@gmail.com>
2026-05-25 02:10:17 +0530

    https://leetcode.com/problems/limit-occurrences-in-sorted-array/submissions/2012052226

---
+++ Leetcode/3940.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+class Solution:
+    def limitOccurrences(self, nums: list[int], k: int) -> list[int]:
+        arr = nums
+        n = len(arr)
+        freq = Counter(arr)
+
+        ans: list[int] = []
+        for key in sorted(freq.keys()):
+            ans.extend([key] * min(freq[key], k))
+
+        return ans
