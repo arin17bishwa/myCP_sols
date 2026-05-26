baf5540fd00b1c056c3c6ad93ffc631f5eefdac5
bishwajit <arin17bishwa@gmail.com>
2026-05-27 01:34:47 +0530

    https://leetcode.com/problems/find-the-duplicate-number/submissions/2013964749

--- Leetcode/287.py
+++ Leetcode/287.py
@@ -1,12 +1,11 @@
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        arr = nums
-        for i in range(len(arr)):
-            temp = arr[i]
-            if arr[abs(temp)] < 0:
-                return abs(temp)
-            arr[abs(temp)] *= -1
+        freq = Counter(nums)
+        for k, v in freq.items():
+            if v > 1:
+                return k
         return -1
