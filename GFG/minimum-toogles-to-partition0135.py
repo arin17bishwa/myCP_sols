1a19507a0fded89e28a82f842e8c1702578cee4c
bishwajit <arin17bishwa@gmail.com>
2026-05-27 01:57:01 +0530

    AC

--- GFG/minimum-toogles-to-partition0135.py
+++ GFG/minimum-toogles-to-partition0135.py
@@ -2,6 +2,7 @@ class Solution:
     def minToggle(self, arr: list[int]) -> int:
         n = len(arr)
         ones = sum(arr)
+
         if ones == 0 or ones == n:
             return 0
 
@@ -17,18 +18,3 @@ class Solution:
             ans = min(ans, running_ones + (n - ones - running_zeroes))
 
         return ans
-
-
-def main():
-    obj = Solution()
-
-    arr = [1, 0, 1, 1, 0]
-    arr = list(map(int, "1 1 1 1 0 1 0 1".split()))
-
-    ans = obj.minToggle(arr)
-
-    # print(ans)
-
-
-if __name__ == "__main__":
-    main()
