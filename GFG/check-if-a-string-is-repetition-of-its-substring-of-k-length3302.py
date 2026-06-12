fbe602f2e32e9c6f239bd39da478d5e7d7744dff
bishwajit <arin17bishwa@gmail.com>
2026-06-12 20:23:22 +0530

    AC

---
+++ GFG/check-if-a-string-is-repetition-of-its-substring-of-k-length3302.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+
+class Solution:
+    def kSubstr(self, s: str, k: int) -> bool:
+        freq = defaultdict(int)
+        n = len(s)
+        if n % k != 0:
+            return False
+
+        for i in range(0, n, k):
+            freq[s[i : i + k]] += 1
+
+        if len(freq) > 2:
+            return False
+        if len(freq) == 1:
+            return True
+        return 1 in freq.values()
+
+
+def main():
+    obj = Solution()
+
+    s = "abcbedabcabc"
+    k = 3
+
+    s = "bdac"
+    k = 2
+
+    s = "abcdabcd"
+    k = 2
+
+    ans = obj.kSubstr(s, k)
+
+    # print(ans)
+
+
+if __name__ == "__main__":
+    main()
