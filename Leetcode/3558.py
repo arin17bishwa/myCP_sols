e667993f8f59573c1951d1eb6c1eb3b9885cf3ed
bishwajit <arin17bishwa@gmail.com>
2026-06-13 23:18:30 +0530

    https://leetcode.com/problems/number-of-ways-to-assign-edge-weights-i/submissions/2032040834

---
+++ Leetcode/3558.py
@@ -0,0 +1,45 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        g = defaultdict(list)
+
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+
+        max_depth = 0
+        vis: set[int] = set()
+
+        def dfs(node: int, curr_depth: int = 0):
+            nonlocal max_depth, vis
+
+            if node in vis:
+                return
+            vis.add(node)
+
+            max_depth = max(max_depth, curr_depth)
+
+            for child in g[node]:
+                dfs(child, curr_depth + 1)
+
+        dfs(1, 0)
+
+        return pow(2, max_depth - 1, 10**9 + 7)
+
+
+def main():
+    obj = Solution()
+
+    arr = [[1, 2]]
+    arr = [[1, 2], [1, 3], [3, 4], [3, 5]]
+
+    ans = obj.assignEdgeWeights(arr)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
