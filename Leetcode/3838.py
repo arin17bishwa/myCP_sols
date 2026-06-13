3b2dfb76afbff493303522a7d7e9a767db2f87ec
bishwajit <arin17bishwa@gmail.com>
2026-06-13 23:01:01 +0530

    https://leetcode.com/problems/weighted-word-mapping/submissions/2032023228

--- Leetcode/3838.py
+++ Leetcode/3838.py
@@ -4,9 +4,9 @@ from typing import List
 class Solution:
     def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
 
-        ans = [
-            chr(122 - (sum(weights[ord(ch) - 97] for ch in word) % 26))
-            for word in words
-        ]
-
-        return "".join(ans)
+        return "".join(
+            [
+                chr(122 - (sum(weights[ord(ch) - 97] for ch in word) % 26))
+                for word in words
+            ]
+        )
