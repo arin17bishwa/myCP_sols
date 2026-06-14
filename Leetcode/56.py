9f34f176275d0320b51ec7a201dadf73d7951941
bishwajit <arin17bishwa@gmail.com>
2026-06-14 20:05:47 +0530

    https://leetcode.com/problems/merge-intervals/submissions/2032911806

--- Leetcode/56.py
+++ Leetcode/56.py
@@ -4,12 +4,19 @@ from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         arr = intervals
+        n = len(arr)
         arr.sort()
-        ans = []
 
-        for start, end in arr:
-            if ans and ans[-1][1] >= start:
-                ans[-1][-1] = max(ans[-1][-1], end)
+        ans = []
+        curr: list[int] = arr[0][:]
+
+        for i in range(1, n):
+            if arr[i][0] <= curr[1]:
+                curr[1] = max(curr[1], arr[i][1])
             else:
-                ans.append([start, end])
+                ans.append(curr[:])
+                curr = arr[i][:]
+
+        ans.append(curr)
+
         return ans
