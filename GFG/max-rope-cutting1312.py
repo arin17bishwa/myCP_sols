89594be896dd6de7a01478b7ac15b6537c0df853
bishwajit <arin17bishwa@gmail.com>
2026-06-17 22:12:18 +0530

    AC

--- GFG/max-rope-cutting1312.py
+++ GFG/max-rope-cutting1312.py
@@ -11,17 +11,3 @@ class Solution:
             return max(k, max(func(k - i) * func(i) for i in range(1, k)))
 
         return max(func(n - i) * func(i) for i in range(1, n))
-
-
-def main():
-    obj = Solution()
-
-    n = 20
-
-    ans = obj.maxProduct(n)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
