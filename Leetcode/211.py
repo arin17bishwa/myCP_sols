d7c0bdf92f235d6bce81a42087f684ead96f8793
bishwajit <arin17bishwa@gmail.com>
2026-06-23 02:19:24 +0530

    https://leetcode.com/problems/design-add-and-search-words-data-structure/submissions/2042657139

---
+++ Leetcode/211.py
@@ -0,0 +1,73 @@
+class Trie:
+    def __init__(self):
+        self.trie: dict[str, Trie] = {}
+        self.is_end: bool = False
+
+    def insert(self, word: str):
+        curr = self
+
+        for ch in word:
+            if ch not in curr.trie:
+                curr.trie[ch] = Trie()
+            curr = curr.trie[ch]
+        curr.is_end = True
+
+    def dfs(self, s: str, idx: int = 0) -> bool:
+        curr = self
+        n = len(s)
+        # if idx == n:
+        #     return True
+
+        for i in range(idx, n):
+            if s[i] == ".":
+                if not curr.trie:
+                    return False
+                for node in curr.trie.values():
+                    x = node.dfs(s, i + 1)
+                    if x:
+                        return True
+                return False
+            else:
+                nxt = curr.trie.get(s[i])
+                if not nxt:
+                    return False
+                curr = nxt
+        return curr and curr.is_end
+
+
+class WordDictionary:
+
+    def __init__(self):
+        self.trie = Trie()
+
+    def addWord(self, word: str) -> None:
+        self.trie.insert(word)
+
+    def search(self, word: str) -> bool:
+        return self.trie.dfs(word, 0)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
+
+
+def main():
+    obj = WordDictionary()
+
+    w1 = ["bad", "dad", "mad"]
+    # w1=['ab']
+    for i in w1:
+        obj.addWord(i)
+
+    w2 = ["bad", ".ad", "b.."]
+    # w2=['a.']
+    for i in w2:
+        ans = obj.search(i)
+        print(ans)
+        pass
+
+
+if __name__ == "__main__":
+    main()
