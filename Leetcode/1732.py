c0401e10d0388d0b513b8053e8f9633ef5b95f47
bishwajit <arin17bishwa@gmail.com>
2026-06-23 22:52:19 +0530

    https://leetcode.com/problems/find-the-highest-altitude/submissions/2043693845

--- Leetcode/1732.py
+++ Leetcode/1732.py
@@ -1,10 +1,7 @@
+from itertools import accumulate
 from typing import List
 
 
 class Solution:
     def largestAltitude(self, gain: List[int]) -> int:
-        ans = curr = 0
-        for i in gain:
-            curr += i
-            ans = max(ans, curr)
-        return ans
+        return max(accumulate(gain, initial=0), default=0)
