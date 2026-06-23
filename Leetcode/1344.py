1c953e5ec3f6e3d8df2fbfaf1e24a315d06967e9
bishwajit <arin17bishwa@gmail.com>
2026-06-23 22:46:13 +0530

    https://leetcode.com/problems/angle-between-hands-of-a-clock/submissions/2043686310

--- Leetcode/1344.py
+++ Leetcode/1344.py
@@ -4,19 +4,3 @@ class Solution:
         hour_angle: float = hour * 30 + minutes * 0.5
         minute_angle = minutes * 6
         return min(abs(hour_angle - minute_angle), 360 - abs(hour_angle - minute_angle))
-
-
-def main():
-    obj = Solution()
-
-    h, m = 12, 30
-    h, m = 3, 30
-    h, m = 3, 15
-
-    ans = obj.angleClock(h, m)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
