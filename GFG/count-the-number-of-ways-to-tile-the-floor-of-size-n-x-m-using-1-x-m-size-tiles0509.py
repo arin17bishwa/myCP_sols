b9225da219e3c02847b609a1dcbf1c919472cf96
bishwajit <arin17bishwa@gmail.com>
2026-06-27 20:11:16 +0530

    AC

---
+++ GFG/count-the-number-of-ways-to-tile-the-floor-of-size-n-x-m-using-1-x-m-size-tiles0509.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countWays(self, n: int, m: int) -> int:
+        mod = 10**9 + 7
+        dp = [0] * (n + 1)
+
+        for i in range(n + 1):
+            if i < m:
+                dp[i] = 1
+            elif i == m:
+                dp[i] = 2
+            else:
+                dp[i] = (dp[i - 1] + dp[i - m]) % mod
+        return dp[-1]
