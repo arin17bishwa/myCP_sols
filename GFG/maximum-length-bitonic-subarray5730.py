f252a8da9d01b41a327978b4810ca20b36410122
bishwajit <arin17bishwa@gmail.com>
2026-07-15 20:42:45 +0530

    AC

--- GFG/maximum-length-bitonic-subarray5730.py
+++ GFG/maximum-length-bitonic-subarray5730.py
@@ -17,22 +17,3 @@ class Solution:
                 dec[i] = dec[i + 1] + 1
 
         return max(inc[i] + dec[i] + 1 for i in range(n))
-
-
-def main():
-    obj = Solution()
-
-    arr = [12, 4, 78, 90, 45, 23]
-    arr = [10, 20, 30, 40]
-    arr = [10, 10, 10, 10]
-    arr = [1, 2, 1, 2, 1]
-
-    arr = list(map(int, "4 3 2 1 1 1 1 1 1 1 2 3 4 5".split()))
-
-    ans = obj.bitonic(arr)
-
-    # print(ans)
-
-
-if __name__ == "__main__":
-    main()
