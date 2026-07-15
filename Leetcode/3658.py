e4006fd637b0a3ab4a1ab3a0f720ba56a7663ae4
bishwajit <arin17bishwa@gmail.com>
2026-07-16 01:44:33 +0530

    https://leetcode.com/problems/gcd-of-odd-and-even-sums/submissions/2069108495

--- Leetcode/3658.py
+++ Leetcode/3658.py
@@ -4,17 +4,3 @@ from math import gcd
 class Solution:
     def gcdOfOddEvenSums(self, n: int) -> int:
         return gcd(sum(range(1, (n << 1) | 1, 2)), sum(range(2, (n << 1) | 1, 2)))
-
-
-def main():
-    obj = Solution()
-
-    n = 4
-
-    ans = obj.gcdOfOddEvenSums(n)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
