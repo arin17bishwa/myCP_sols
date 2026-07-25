bebbf97bfcfc9987eab6da8018a8ba40e5b8c26f
bishwajit <arin17bishwa@gmail.com>
2026-07-26 01:36:04 +0530

    https://leetcode.com/problems/maximum-product-of-two-digits/submissions/2081205527

--- Leetcode/3536.py
+++ Leetcode/3536.py
@@ -1,10 +1,4 @@
 class Solution:
     def maxProduct(self, n: int) -> int:
-        digs = list(map(int, str(n)))
-        m = len(digs)
-        ans = 0
-        for i in range(m - 1):
-            for j in range(i + 1, m):
-                ans = max(ans, digs[i] * digs[j])
-
-        return ans
+        digs = sorted(map(int, str(n)), reverse=True)
+        return digs[0] * digs[1]
