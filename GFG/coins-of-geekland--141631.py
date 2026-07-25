4e5bcaa5f53af352a84fb9f5c63db9e21635de05
bishwajit <arin17bishwa@gmail.com>
2026-07-26 00:15:25 +0530

    AC

---
+++ GFG/coins-of-geekland--141631.py
@@ -0,0 +1,46 @@
+class Solution:
+    def maximumSum(self, mat: list[list[int]], k: int) -> int:
+        n = len(mat)
+        # print(*mat, sep="\n")
+        # print("-" * 30)
+
+        for i in range(1, n):
+            mat[0][i] += mat[0][i - 1]
+            mat[i][0] += mat[i - 1][0]
+
+        for i in range(1, n):
+            for j in range(1, n):
+                mat[i][j] += mat[i - 1][j] + mat[i][j - 1] - mat[i - 1][j - 1]
+
+        ans = mat[k - 1][k - 1]
+
+        for i in range(k - 1, n):
+            for j in range(k - 1, n):
+                ans = max(
+                    ans,
+                    mat[i][j]
+                    - (0 if i == k - 1 else mat[i - k][j])
+                    - (0 if j == k - 1 else mat[i][j - k])
+                    + (0 if (i == k - 1 or j == k - 1) else mat[i - k][j - k]),
+                )
+
+        # print(*mat, sep="\n")
+        return ans
+
+
+def main():
+    obj = Solution()
+
+    arr = [[1, 2, -1, 4], [-8, -3, 4, 2], [3, 8, 10, -8], [-4, -1, 1, 7]]
+    k = 3
+
+    arr = [[-1, 2], [3, -4]]
+    k = 1
+
+    ans = obj.maximumSum(arr, k)
+
+    # print(ans)
+
+
+if __name__ == "__main__":
+    main()
