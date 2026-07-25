21a7fa7299f416cfeccf385ade217d4597b317d2
bishwajit <arin17bishwa@gmail.com>
2026-07-26 03:03:23 +0530

    https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/submissions/2081246797

--- Leetcode/1404.py
+++ Leetcode/1404.py
@@ -1,8 +1,8 @@
 class Solution:
     def numSteps(self, s: str) -> int:
-        ops: int = 0
         n: int = len(s)
-        carry: int = 0
+        ops = carry = 0
+
         for i in range(n - 1, 0, -1):
 
             if ((s[i] == "1") + carry) & 1:
@@ -12,19 +12,3 @@ class Solution:
                 ops += 1
 
         return ops + carry
-
-
-def main():
-    obj = Solution()
-
-    s = "1101"
-    s = "10"
-    s = "1"
-
-    ans = obj.numSteps(s)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
