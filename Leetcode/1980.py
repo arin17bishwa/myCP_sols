1d9a4288c4980e6be8f0a3d07e32988174ce209d
bishwajit <arin17bishwa@gmail.com>
2026-08-04 23:52:26 +0530

    https://leetcode.com/problems/find-unique-binary-string/submissions/2094464758

--- Leetcode/1980.py
+++ Leetcode/1980.py
@@ -3,10 +3,9 @@ from typing import List
 
 class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
-        arr = [int(i, 2) for i in nums]
-        arr.sort()
-        n = len(arr)
-        for i in range(n):
-            if i != arr[i]:
+        n = len(nums[0])
+        seen: set[int] = set(int(i, 2) for i in nums)
+        for i in range(1 << 16):
+            if i not in seen:
                 return bin(i)[2:].zfill(n)
-        return bin(n)[2:].zfill(n)
+        return bin(1 << 16)
