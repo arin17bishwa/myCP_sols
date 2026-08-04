fb39b15c3e4f90d5b85c2e35850db3a3c08f22f2
bishwajit <arin17bishwa@gmail.com>
2026-08-04 22:00:56 +0530

    AC

---
+++ GFG/pairs-with-difference-less-than-k1348.py
@@ -0,0 +1,42 @@
+class Solution:
+    def countPairs(self, arr: list[int], k: int) -> int:
+        arr.sort()
+        n = len(arr)
+
+        def search(x: int) -> int:
+            nonlocal arr, n
+            lo, hi = 0, n - 1
+            fin = 0
+
+            while lo <= hi:
+                mid = (lo + hi) >> 1
+                if arr[mid] < x:
+                    lo = mid + 1
+                    fin = mid
+                else:
+                    hi = mid - 1
+            return fin
+
+        ans = 0
+        for i in range(n):
+            ans += max(0, search(arr[i] + k) - i)
+
+        return ans
+
+
+def main():
+    obj = Solution()
+
+    arr = [1, 10, 4, 2]
+    k = 3
+
+    arr = [2, 3, 4]
+    k = 5
+
+    ans = obj.countPairs(arr, k)
+
+    # print(ans)
+
+
+if __name__ == "__main__":
+    main()
