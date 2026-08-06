e079c5b5833e427bbedd38a5f7b520f0e3d94c1b
bishwajit <arin17bishwa@gmail.com>
2026-08-07 00:06:21 +0530

    https://leetcode.com/problems/single-number/submissions/2097159164

--- Leetcode/136.py
+++ Leetcode/136.py
@@ -1,9 +1,7 @@
+from functools import reduce
 from typing import List
 
 
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans: int = 0
-        for i in nums:
-            ans ^= i
-        return ans
+        return reduce(lambda x, y: x ^ y, nums, 0)
