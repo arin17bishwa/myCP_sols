4692e00471f5ea22611017b9892e79ed5f413564
bishwajit <arin17bishwa@gmail.com>
2026-08-06 23:47:51 +0530

    AC

--- GFG/minimum-steps-to-get-desired-array5519.py
+++ GFG/minimum-steps-to-get-desired-array5519.py
@@ -11,19 +11,3 @@ class Solution:
             mx >>= 1
 
         return ans
-
-
-def main():
-    obj = Solution()
-
-    arr = [16, 16, 16]
-
-    arr = [2, 3]
-
-    ans = obj.countMinOperations(arr)
-
-    # print(ans)
-
-
-if __name__ == "__main__":
-    main()
