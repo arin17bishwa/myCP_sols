65f88678900467ec4d8f73695555665708688231
bishwajit <arin17bishwa@gmail.com>
2026-08-08 18:29:41 +0530

    https://leetcode.com/problems/counting-bits/submissions/2099112427

--- Leetcode/JavaSolutions/Solution338.java
+++ Leetcode/JavaSolutions/Solution338.java
@@ -7,20 +7,17 @@ public class Solution338 {
         public int[] countBits(int n) {
             int[] ans = new int[n + 1];
 
-            for (int i = 0; i <= n; i++) {
-                ans[i] = countSetBits(i);
+            if (n == 0)
+                return ans;
+
+            ans[1] = 1;
+
+            for (int i = 2; i <= n; i++) {
+                ans[i] = ans[i >> 1] + (i & 1);
             }
             return ans;
         }
 
-        public int countSetBits(Integer n) {
-            int ans = 0;
-            while (n > 0) {
-                ans += (n & 1);
-                n >>= 1;
-            }
-            return ans;
-        }
     }
 
     public static void main(String[] args) {
