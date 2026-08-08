f4f878ee5d9a7fe32a96223290dc63be9191c182
bishwajit <arin17bishwa@gmail.com>
2026-08-08 18:26:30 +0530

    https://leetcode.com/problems/counting-bits/submissions/2099109301

--- Leetcode/338.py
+++ Leetcode/338.py
@@ -3,13 +3,12 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans: list[int] = []
+        if n == 0:
+            return [0]
+        ans: list[int] = [0] * (n + 1)
+        ans[1] = 1
 
-        for i in range(n + 1):
-            cnt=0
-            while i:
-                cnt+=i&1
-                i>>=1
-            ans.append(cnt)
+        for i in range(2, n + 1):
+            ans[i] = ans[i >> 1] + (i & 1)
 
         return ans
