d6918bf77e8d775e1a242738afc5fca01725f1d7
bishwajit <arin17bishwa@gmail.com>
2026-08-23 19:10:55 +0530

    https://leetcode.com/problems/sum-game/submissions/2117394289

---
+++ Leetcode/1927.py
@@ -0,0 +1,48 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        n = len(num)
+        left_q = right_q = 0
+        left_sm = right_sm = 0
+        for i in range(n // 2):
+            if num[i] == "?":
+                left_q += 1
+            else:
+                left_sm += int(num[i])
+
+        for i in range(n // 2, n):
+            if num[i] == "?":
+                right_q += 1
+            else:
+                right_sm += int(num[i])
+
+        print(left_sm, left_q)
+        print(right_sm, right_q)
+
+        if left_q + right_q == 0:
+            return left_sm != right_sm
+
+        total_q = left_q + right_q
+
+        if total_q & 1:
+            return True
+        else:
+            if left_q == right_q:
+                return left_sm != right_sm
+            else:
+                return 9 * (left_q - right_q) != 2 * (right_sm - left_sm)
+
+
+def main():
+    obj = Solution()
+
+    arr = "5023"
+    arr = "25??"
+    arr = "?3295???"
+
+    ans = obj.sumGame(arr)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
