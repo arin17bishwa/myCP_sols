901f81fb37399121d95438d1741da1bc4e305869
bishwajit <arin17bishwa@gmail.com>
2026-09-04 20:19:50 +0530

    https://leetcode.com/problems/smallest-stable-index-i/submissions/2130812045

--- Leetcode/3903.py
+++ Leetcode/3903.py
@@ -2,14 +2,14 @@ class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         arr = nums
         n = len(arr)
+        mn = arr[:]
         mx = arr[0]
-        suffix = arr[:]
-
         for i in range(n - 2, -1, -1):
-            suffix[i] = min(suffix[i], suffix[i + 1])
+            mn[i] = min(mn[i], mn[i + 1])
 
         for i in range(n):
             mx = max(mx, arr[i])
-            if mx - suffix[i] <= k:
+            if mx - mn[i] <= k:
                 return i
+
         return -1
