73f07c5a4c08b2fe11843dc15b911da50024162f
bishwajit <arin17bishwa@gmail.com>
2026-09-13 16:05:43 +0530

    https://leetcode.com/problems/count-commas-in-range-ii/submissions/2140509631

--- Leetcode/3871.py
+++ Leetcode/3871.py
@@ -1,27 +1,9 @@
 class Solution:
     def countCommas(self, n: int) -> int:
         ans: int = 0
-        exp = 1
+        exp: int = 1
         while pow(10, 3 * exp) <= n:
             ans += (exp - 1) * (pow(10, 3 * exp) - pow(10, 3 * (exp - 1)))
             exp += 1
         exp -= 1
-        ans += exp * (n - pow(10, 3 * exp) + 1)
-        return ans
-
-
-def main():
-    obj = Solution()
-
-    n = 1002
-    n = 998
-    n = 1002000999
-    n = 1000
-
-    ans = obj.countCommas(n)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
+        return ans + exp * (n - pow(10, 3 * exp) + 1)
