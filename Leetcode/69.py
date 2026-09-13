0be76571aa59ec2a677a3f3ee73c33549b9691fa
bishwajit <arin17bishwa@gmail.com>
2026-09-13 17:01:20 +0530

    https://leetcode.com/problems/sqrtx/submissions/2140551505

--- Leetcode/69.py
+++ Leetcode/69.py
@@ -1,14 +1,35 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        lo, hi = 0, x + 1
-        ans = hi
-        e = 1e-6
-        while (hi - lo) >= e:
-            mid = (lo + hi) / 2
+        lo, hi = 0, x
+        ans = 0
+        while lo <= hi:
+            mid = (lo + hi) >> 1
             k = mid * mid
-            if (k - x) >= e:
-                hi = mid
+
+            if k > x:
+                hi = mid - 1
+            elif k < x:
                 ans = mid
+                lo = mid + 1
             else:
-                lo = mid
+                return mid
         return int(ans)
+
+
+def main():
+    obj = Solution()
+
+    n = 4
+    n = 8
+    n = 1
+    # n = 36
+    # n = 0
+    # n = 2147395599
+
+    ans = obj.mySqrt(n)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
