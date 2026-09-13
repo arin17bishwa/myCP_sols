cd1e66953af6676915228abe41426a468f7ebb8a
bishwajit <arin17bishwa@gmail.com>
2026-09-13 16:52:53 +0530

    https://leetcode.com/problems/sqrtx/submissions/2140545413

--- Leetcode/69.py
+++ Leetcode/69.py
@@ -5,7 +5,6 @@ class Solution:
         e = 1e-6
         while (hi - lo) >= e:
             mid = (lo + hi) / 2
-            # print(lo, hi, mid)
             k = mid * mid
             if (k - x) >= e:
                 hi = mid
@@ -13,22 +12,3 @@ class Solution:
             else:
                 lo = mid
         return int(ans)
-
-
-def main():
-    obj = Solution()
-
-    n = 4
-    n = 8
-    n = 1
-    n = 36
-    n = 0
-    n = 2147395599
-
-    ans = obj.mySqrt(n)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
