48fa0949fd9bfbffc4e925114b948437708936f1
bishwajit <arin17bishwa@gmail.com>
2026-09-13 18:22:43 +0530

    https://leetcode.com/problems/reorganize-string/submissions/2140611060

--- Leetcode/767.py
+++ Leetcode/767.py
@@ -3,20 +3,35 @@ from collections import Counter
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        freq = Counter(s)
         n = len(s)
+        freq = Counter(s)
+        mx = max(freq.values())
 
-        if any(v > (1 + n) // 2 for v in freq.values()):
+        if (n + 1) >> 1 < mx:
             return ""
 
-        chars = sorted(freq.keys(), key=lambda x: freq[x], reverse=True)
         ans: list[str] = [""] * n
-        curr_idx = 0
-        for ch in chars:
-            for _ in range(freq[ch]):
-                if curr_idx >= n:
-                    curr_idx = 1
-                ans[curr_idx] = ch
-                curr_idx += 2
+        it = iter(list(range(0, n, 2)) + list(range(1, n, 2)))
+
+        for k, v in sorted(freq.items(), key=lambda x: -x[1]):
+            while v:
+                ans[next(it)] = k
+                v -= 1
 
         return "".join(ans)
+
+
+def main():
+    obj = Solution()
+
+    s = "aab"
+    s = "aaab"
+    s = "baaba"
+
+    ans = obj.reorganizeString(s)
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
