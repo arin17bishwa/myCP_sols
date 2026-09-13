c427e344398ac3aac6694ae4357331c7d6fb3756
bishwajit <arin17bishwa@gmail.com>
2026-09-13 16:19:48 +0530

    https://leetcode.com/problems/existence-of-a-substring-in-a-string-and-its-reverse/submissions/2140520651

--- Leetcode/3083.py
+++ Leetcode/3083.py
@@ -1,24 +1,8 @@
 class Solution:
     def isSubstringPresent(self, s: str) -> bool:
         n = len(s)
-        seen: set[str] = set((s[i + 1] + s[i] for i in range(n - 1)))
+        seen: set[str] = set((s[i : i + 2][::-1] for i in range(n - 1)))
         for i in range(n - 1):
             if s[i : i + 2] in seen:
                 return True
         return False
-
-
-def main():
-    obj = Solution()
-
-    s = "leetcode"
-    s = "abcba"
-    s = "abcd"
-
-    ans = obj.isSubstringPresent(s)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
