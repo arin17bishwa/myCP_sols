236263ab5592496647da19742961e6d671054620
bishwajit <arin17bishwa@gmail.com>
2026-09-13 17:20:33 +0530

    https://leetcode.com/problems/sqrtx/submissions/2140565503

--- Leetcode/69.py
+++ Leetcode/69.py
@@ -4,22 +4,3 @@ class Solution:
         while r * r > x:
             r = (r + x // r) >> 1
         return r
-
-
-def main():
-    obj = Solution()
-
-    n = 4
-    n = 8
-    n = 1
-    n = 36
-    n = 0
-    n = 2147395599
-
-    ans = obj.mySqrt(n)
-
-    print(ans)
-
-
-if __name__ == "__main__":
-    main()
