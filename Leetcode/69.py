ad30178d206c9e29dc31b5244fb1f32d0e326515
bishwajit <arin17bishwa@gmail.com>
2026-09-13 16:52:23 +0530

    https://leetcode.com/problems/sqrtx/submissions/2140545028

--- Leetcode/69.py
+++ Leetcode/69.py
@@ -1,7 +1,7 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         lo, hi = 0, x + 1
-        ans = lo
+        ans = hi
         e = 1e-6
         while (hi - lo) >= e:
             mid = (lo + hi) / 2
