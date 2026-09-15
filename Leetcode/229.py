045819a6d7d1dd96e5fb1d79e22d968428b52d85
bishwajit <arin17bishwa@gmail.com>
2026-09-16 04:43:33 +0530

    https://leetcode.com/problems/majority-element-ii/submissions/2143109051

--- Leetcode/229.py
+++ Leetcode/229.py
@@ -1,8 +1,28 @@
-from collections import Counter
-from typing import List
-
-
 class Solution:
-    def majorityElement(self, nums: List[int]) -> list[int]:
-        freq = Counter(nums)
-        return [k for k, v in freq.items() if v > len(nums) // 3]
+    def majorityElement(self, nums: list[int]) -> list[int]:
+        arr = nums
+        n = len(arr)
+        candidate1 = candidate2 = -(10**10)
+        cnt1 = cnt2 = 0
+
+        for i in arr:
+            if cnt1 == 0 and candidate2 != i:
+                cnt1 = 1
+                candidate1 = i
+            elif cnt2 == 0 and candidate1 != i:
+                cnt2 = 1
+                candidate2 = i
+            elif candidate1 == i:
+                cnt1 += 1
+            elif candidate2 == i:
+                cnt2 += 1
+            else:
+                cnt1 -= 1
+                cnt2 -= 1
+        ans: list[int] = []
+
+        for candidate in (candidate1, candidate2):
+            if arr.count(candidate) > n // 3:
+                ans.append(candidate)
+
+        return ans
