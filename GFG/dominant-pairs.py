a8b734b01caa162b22cf31b01485693c212898b9
bishwajit <arin17bishwa@gmail.com>
2026-09-16 21:44:27 +0530

    AC

--- GFG/dominant-pairs.py
+++ GFG/dominant-pairs.py
@@ -13,19 +13,3 @@ class Solution:
             ans += m - x
 
         return ans
-
-
-def main():
-    obj = Solution()
-
-    arr = [10, 2, 2, 1]
-    arr = [10, 8, 2, 1, 1, 2]
-    arr = [5, 1, 1, 1, 1, 1, 1, 1]
-
-    ans = obj.dominantPairs(arr)
-
-    # print(ans)
-
-
-if __name__ == "__main__":
-    main()
